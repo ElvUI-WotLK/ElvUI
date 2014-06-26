c73788dfc48445a5610138f7aabb54b48f8f6b0d
Bunny67 <zolov67@yandex.ru>
2014-06-26 09:22:43 +0400

    Update Taxi Skins

--- ElvUI/modules/skins/blizzard/taxi.lua
+++ ElvUI/modules/skins/blizzard/taxi.lua
@@ -2,18 +2,18 @@ local E, L, V, P, G = unpack(select(2, ...));
 local S = E:GetModule('Skins');
 
 local function LoadSkin()
-	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.taxi ~= true then return end
-
-	TaxiPortrait:Kill();
-
+	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.taxi ~= true then return; end
+	
 	TaxiFrame:StripTextures();
 	TaxiFrame:CreateBackdrop('Transparent');
-	TaxiFrame.backdrop:Point('TOPLEFT', 10, -12);
-	TaxiFrame.backdrop:Point('BOTTOMRIGHT', -33, 76);
-
-	TaxiRouteMap:CreateBackdrop('Default');
-
+	TaxiFrame.backdrop:Point('TOPLEFT', 11, -12);
+	TaxiFrame.backdrop:Point('BOTTOMRIGHT', -34, 75);
+	
+	TaxiPortrait:Kill();
+	
 	S:HandleCloseButton(TaxiCloseButton);
+	
+	TaxiRouteMap:CreateBackdrop('Default');
 end
 
 S:RegisterSkin('ElvUI', LoadSkin);
