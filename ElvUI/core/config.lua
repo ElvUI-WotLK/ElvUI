50492502aa9316979190141eb164016a19cdb365
Bunny67 <zolov67@yandex.ru>
2015-05-14 20:27:22 +0300

    fix russian translation

--- ElvUI/core/config.lua
+++ ElvUI/core/config.lua
@@ -461,43 +461,67 @@ function E:CreateMoverPopup()
 	end)
 	S:HandleButton(resetButton)
 	-- Up Button
-	local upButton = CreateFrame('Button', nudgeFrame:GetName()..'PrevButton', nudgeFrame, 'OptionsButtonTemplate')
-	upButton:SetSize(36, 22)
-	upButton:SetPoint('BOTTOMRIGHT', nudgeFrame, 'BOTTOM', -10, 6)
-	upButton:SetScript('OnClick', function()
-		yOffset:SetText(yOffset.currentValue + 1)
-		yOffset:GetScript('OnEnterPressed')(yOffset)
-	end)
-	S:HandleButton(upButton)
-	_G[upButton:GetName() .. "Text"]:SetText('Вверх')
+	local upButton = CreateFrame("Button", nudgeFrame:GetName().."PrevButton", nudgeFrame);
+	upButton:SetSize(26, 26);
+	upButton:SetPoint("BOTTOMRIGHT", nudgeFrame, "BOTTOM", -6, 4);
+	upButton:SetScript("OnClick", function()
+		yOffset:SetText(yOffset.currentValue + 1);
+		yOffset:GetScript("OnEnterPressed")(yOffset);
+	end);
+	upButton.icon = upButton:CreateTexture(nil, "ARTWORK");
+	upButton.icon:SetSize(13, 13);
+	upButton.icon:SetPoint("CENTER");
+	upButton.icon:SetTexture([[Interface\AddOns\ElvUI\media\textures\SquareButtonTextures.blp]]);
+	upButton.icon:SetTexCoord(0.01562500, 0.20312500, 0.01562500, 0.20312500);
+	
+	S:SquareButton_SetIcon(upButton, "UP");
+	S:HandleButton(upButton);
 	-- Down Button
-	local downButton = CreateFrame('Button', nudgeFrame:GetName()..'DownButton', nudgeFrame, 'OptionsButtonTemplate')
-	downButton:SetSize(36, 22)
-	downButton:SetPoint('BOTTOMLEFT', nudgeFrame, 'BOTTOM', 10, 6)
-	downButton:SetScript('OnClick', function()
-		yOffset:SetText(yOffset.currentValue - 1)
-		yOffset:GetScript('OnEnterPressed')(yOffset)
-	end)
-	S:HandleButton(downButton)
-	_G[downButton:GetName() .. "Text"]:SetText('Вниз')
+	local downButton = CreateFrame("Button", nudgeFrame:GetName().."DownButton", nudgeFrame);
+	downButton:SetSize(26, 26);
+	downButton:SetPoint("BOTTOMLEFT", nudgeFrame, "BOTTOM", 6, 4);
+	downButton:SetScript("OnClick", function()
+		yOffset:SetText(yOffset.currentValue - 1);
+		yOffset:GetScript("OnEnterPressed")(yOffset);
+	end);
+	downButton.icon = downButton:CreateTexture(nil, "ARTWORK");
+	downButton.icon:SetSize(13, 13);
+	downButton.icon:SetPoint("CENTER");
+	downButton.icon:SetTexture([[Interface\AddOns\ElvUI\media\textures\SquareButtonTextures.blp]]);
+	downButton.icon:SetTexCoord(0.01562500, 0.20312500, 0.01562500, 0.20312500);
+	
+	S:SquareButton_SetIcon(downButton, "DOWN");
+	S:HandleButton(downButton);
 	-- Left Button
-	local leftButton = CreateFrame('Button', nudgeFrame:GetName()..'LeftButton', nudgeFrame, 'OptionsButtonTemplate')
-	leftButton:SetSize(36, 22)
-	leftButton:SetPoint('RIGHT', upButton, 'LEFT', -10, 0)
-	leftButton:SetScript('OnClick', function()
-		xOffset:SetText(xOffset.currentValue - 1)
-		xOffset:GetScript('OnEnterPressed')(xOffset)
-	end)
-	S:HandleButton(leftButton)		
-	_G[leftButton:GetName() .. "Text"]:SetText('Влево')
+	local leftButton = CreateFrame("Button", nudgeFrame:GetName().."LeftButton", nudgeFrame);
+	leftButton:SetSize(26, 26);
+	leftButton:SetPoint("RIGHT", upButton, "LEFT", -6, 0);
+	leftButton:SetScript("OnClick", function()
+		xOffset:SetText(xOffset.currentValue - 1);
+		xOffset:GetScript("OnEnterPressed")(xOffset);
+	end);
+	leftButton.icon = leftButton:CreateTexture(nil, "ARTWORK");
+	leftButton.icon:SetSize(13, 13);
+	leftButton.icon:SetPoint("CENTER");
+	leftButton.icon:SetTexture([[Interface\AddOns\ElvUI\media\textures\SquareButtonTextures.blp]]);
+	leftButton.icon:SetTexCoord(0.01562500, 0.20312500, 0.01562500, 0.20312500);
+	
+	S:SquareButton_SetIcon(leftButton, "LEFT");
+	S:HandleButton(leftButton);
 	-- Right Button
-	local rightButton = CreateFrame('Button', nudgeFrame:GetName()..'RightButton', nudgeFrame, 'OptionsButtonTemplate')
-	rightButton:SetSize(36, 22)
-	rightButton:SetPoint('LEFT', downButton, 'RIGHT', 10, 0)
-	rightButton:SetScript('OnClick', function()
-		xOffset:SetText(xOffset.currentValue + 1)
-		xOffset:GetScript('OnEnterPressed')(xOffset)
-	end)
-	S:HandleButton(rightButton)
-	_G[rightButton:GetName() .. "Text"]:SetText('Вправо')
+	local rightButton = CreateFrame("Button", nudgeFrame:GetName().."RightButton", nudgeFrame);
+	rightButton:SetSize(26, 26);
+	rightButton:SetPoint("LEFT", downButton, "RIGHT", 6, 0);
+	rightButton:SetScript("OnClick", function()
+		xOffset:SetText(xOffset.currentValue + 1);
+		xOffset:GetScript("OnEnterPressed")(xOffset);
+	end);
+	rightButton.icon = rightButton:CreateTexture(nil, "ARTWORK");
+	rightButton.icon:SetSize(13, 13);
+	rightButton.icon:SetPoint("CENTER");
+	rightButton.icon:SetTexture([[Interface\AddOns\ElvUI\media\textures\SquareButtonTextures.blp]]);
+	rightButton.icon:SetTexCoord(0.01562500, 0.20312500, 0.01562500, 0.20312500);
+	
+	S:SquareButton_SetIcon(rightButton, "RIGHT");
+	S:HandleButton(rightButton);
 end
