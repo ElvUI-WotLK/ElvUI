a8b7ea2ec2178de6feecd03f33d95c699ac2e594
Bunny67 <zolov67@yandex.ru>
2015-09-06 09:40:09 +0300

    added sounds

--- ElvUI/media/sharedmedia.lua
+++ ElvUI/media/sharedmedia.lua
@@ -10,4 +10,13 @@ LSM:Register('border', 'ElvUI GlowBorder', [[Interface\AddOns\ElvUI\media\textur
 LSM:Register('font','ElvUI Font', [[Interface\AddOns\ElvUI\media\fonts\PT_Sans_Narrow.ttf]], LSM.LOCALE_BIT_ruRU + LSM.LOCALE_BIT_western)
 LSM:Register('font', 'ElvUI Pixel', [[Interface\AddOns\ElvUI\media\fonts\Homespun.ttf]], LSM.LOCALE_BIT_ruRU + LSM.LOCALE_BIT_western)
 LSM:Register('font','ElvUI Combat', [[Interface\AddOns\ElvUI\media\fonts\DieDieDie.ttf]], LSM.LOCALE_BIT_ruRU + LSM.LOCALE_BIT_western)
-LSM:Register('sound', 'ElvUI Aska', [[Interface\AddOns\ElvUI\media\sounds\sndIncMsg.wav]])
+LSM:Register('sound', 'ElvUI Aska', [[Interface\AddOns\ElvUI\media\sounds\sndIncMsg.wav]]);
+LSM:Register("sound", "Awww Crap", [[Interface\AddOns\ElvUI\media\sounds\awwcrap.ogg]]);
+LSM:Register("sound", "BBQ Ass", [[Interface\AddOns\ElvUI\media\sounds\bbqass.ogg]]);
+LSM:Register("sound", "Big Yankie Devil", [[Interface\AddOns\ElvUI\media\sounds\yankiebangbang.ogg]]);
+LSM:Register("sound", "Dumb Shit", [[Interface\AddOns\ElvUI\media\sounds\dumbshit.ogg]]);
+LSM:Register("sound", "Mama Weekends", [[Interface\AddOns\ElvUI\media\sounds\mamaweekends.ogg]]);
+LSM:Register("sound", "Runaway Fast", [[Interface\AddOns\ElvUI\media\sounds\runfast.ogg]]);
+LSM:Register("sound", "Stop Running", [[Interface\AddOns\ElvUI\media\sounds\stoprunningslimball.ogg]]);
+LSM:Register("sound", "Warning", [[Interface\AddOns\ElvUI\media\sounds\warning.ogg]]);
+LSM:Register("sound", "Whisper Alert", [[Interface\AddOns\ElvUI\media\sounds\whisper.ogg]])
