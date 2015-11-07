c472ab27d2a8982b4d1c177dfdfe18b801f962f1
Bunny67 <zolov67@yandex.ru>
2015-11-07 10:51:57 +0300

    update captureBar

--- ElvUI/modules/blizzard/capturebar.lua
+++ ElvUI/modules/blizzard/capturebar.lua
@@ -1,25 +1,17 @@
-local E, L, DF = unpack(select(2, ...))
-local B = E:GetModule('Blizzard');
+local E, L, DF = unpack(select(2, ...));
+local B = E:GetModule("Blizzard");
 
-local function CaptureUpdate()
-	if NUM_EXTENDED_UI_FRAMES then
-		local captureBar;
-		for i=1, NUM_EXTENDED_UI_FRAMES do
-			captureBar = _G['WorldStateCaptureBar' .. i];
-
-			if captureBar and captureBar:IsVisible() then
-				captureBar:ClearAllPoints();
-				
-				if( i == 1 ) then
-					captureBar:Point('TOP', E.UIParent, 'TOP', 0, -170);
-				else
-					captureBar:Point('TOPLEFT', _G['WorldStateCaptureBar' .. i - 1], 'TOPLEFT', 0, -45);
-				end
-			end
+function B:WorldStateAlwaysUpFrame_Update()
+	local captureBar;
+	for i = 1, NUM_EXTENDED_UI_FRAMES do
+		captureBar = _G["WorldStateCaptureBar" .. i];
+		if(captureBar and captureBar:IsShown()) then
+			captureBar:ClearAllPoints();
+			captureBar:Point("TOP", E.UIParent, "TOP", 0, -170);
 		end
 	end
 end
 
 function B:PositionCaptureBar()
-	self:SecureHook('WorldStateAlwaysUpFrame_Update', CaptureUpdate);
+	self:SecureHook("WorldStateAlwaysUpFrame_Update");
 end
