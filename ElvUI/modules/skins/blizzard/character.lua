6209a5fe64310a67ba7f444612eab3ae3475b737
Bunny67 <zolov67@yandex.ru>
2016-08-11 17:20:30 +0300

    Update Character Flyout

--- ElvUI/modules/skins/blizzard/character.lua
+++ ElvUI/modules/skins/blizzard/character.lua
@@ -37,32 +37,30 @@ local function LoadSkin()
 	S:HandleButton(GearManagerDialogSaveSet)
 	
 	PaperDollFrameItemFlyoutHighlight:Kill()
-	local function SkinItemFlyouts()
-		PaperDollFrameItemFlyoutButtons:StripTextures()
-		
-		for i=1, 25 do 
-			local button = _G["PaperDollFrameItemFlyoutButtons"..i]
-			local icon = _G["PaperDollFrameItemFlyoutButtons"..i.."IconTexture"]
-			if button then
-				button:GetNormalTexture():SetTexture(nil)
-				button:SetFrameLevel(button:GetFrameLevel() + 2)
-				button:StyleButton(false)
-				
-				icon:SetTexCoord(unpack(E.TexCoords))
-				icon:ClearAllPoints()
-				icon:Point("TOPLEFT", 2, -2)
-				icon:Point("BOTTOMRIGHT", -2, 2)
-				
-				if not button.backdrop then
-					button:CreateBackdrop("Default")
-					button.backdrop:SetAllPoints()			
-				end
-			end
+	local function SkinItemFlyouts(button)
+		if(not button.isSkinned) then
+			PaperDollFrameItemFlyoutButtons:StripTextures();
+			button.icon = _G[button:GetName() .. "IconTexture"];
+
+			button:GetNormalTexture():SetTexture(nil);
+			button:SetTemplate("Default");
+			button:StyleButton(false);
+
+			button.icon:SetInside();
+			button.icon:SetTexCoord(unpack(E.TexCoords));
 		end
+
+		local location = button.location;
+		if(not location) then return; end
+		if(location >= PDFITEMFLYOUT_FIRST_SPECIAL_LOCATION) then return; end
+
+		local id = EquipmentManager_GetItemInfoByLocation(location);
+		local _, _, quality = GetItemInfo(id);
+		local r, g, b = GetItemQualityColor(quality);
+
+		button:SetBackdropBorderColor(r, g, b);
 	end
-	
-	PaperDollFrameItemFlyout:HookScript('OnShow', SkinItemFlyouts)
-	hooksecurefunc('PaperDollItemSlotButton_OnShow', SkinItemFlyouts)
+	hooksecurefunc("PaperDollFrameItemFlyout_DisplayButton", SkinItemFlyouts);
 	
 	GearManagerDialogPopup:StripTextures()
 	GearManagerDialogPopup:CreateBackdrop("Transparent")
