065333e96b474020b273ac858eb64b7c46e3e7da
Bunny67 <zolov67@yandex.ru>
2016-12-04 18:08:30 +0300

    update

--- ElvUI/modules/skins/skins.lua
+++ ElvUI/modules/skins/skins.lua
@@ -2,7 +2,7 @@ local E, L, V, P, G = unpack(select(2, ...));
 local S = E:NewModule("Skins", "AceHook-3.0", "AceEvent-3.0");
 
 local _G = _G;
-local unpack, assert, pairs, select, type, pcall = unpack, assert, pairs, select, type, pcall;
+local unpack, assert, pairs, ipairs, select, type, pcall = unpack, assert, pairs, ipairs, select, type, pcall;
 local tinsert, wipe = table.insert, table.wipe;
 
 local CreateFrame = CreateFrame;
@@ -16,7 +16,7 @@ S.addonsToLoad = {};
 S.nonAddonsToLoad = {};
 S.allowBypass = {};
 S.addonCallbacks = {};
-S.nonAddonCallbacks = {};
+S.nonAddonCallbacks = {["CallPriority"] = {}};
 
 local find = string.find;
 
@@ -465,14 +465,15 @@ end
 
 function S:ADDON_LOADED(event, addon)
 	if(self.allowBypass[addon]) then
-		if(S.addonsToLoad[addon]) then
+		if(self.addonsToLoad[addon]) then
 			--Load addons using the old deprecated register method
-			S.addonsToLoad[addon]();
-			S.addonsToLoad[addon] = nil;
-		elseif(S.addonCallbacks[addon]) then
+			self.addonsToLoad[addon]();
+			self.addonsToLoad[addon] = nil;
+		elseif(self.addonCallbacks[addon]) then
 			--Fire events to the skins that rely on this addon
-			for event in pairs(S.addonCallbacks[addon]) do
-				S.addonCallbacks[addon][event] = nil;
+			for index, event in ipairs(self.addonCallbacks[addon]["CallPriority"]) do
+				self.addonCallbacks[addon][event] = nil;
+				self.addonCallbacks[addon]["CallPriority"][index] = nil;
 				E.callbacks:Fire(event);
 			end
 		end
@@ -481,12 +482,13 @@ function S:ADDON_LOADED(event, addon)
 
 	if(not E.initialized) then return; end
 
-	if(S.addonsToLoad[addon]) then
-		S.addonsToLoad[addon]();
-		S.addonsToLoad[addon] = nil;
-	elseif S.addonCallbacks[addon] then
-		for event in pairs(S.addonCallbacks[addon]) do
-			S.addonCallbacks[addon][event] = nil;
+	if(self.addonsToLoad[addon]) then
+		self.addonsToLoad[addon]();
+		self.addonsToLoad[addon] = nil;
+	elseif self.addonCallbacks[addon] then
+		for index, event in ipairs(self.addonCallbacks[addon]["CallPriority"]) do
+			self.addonCallbacks[addon][event] = nil;
+			self.addonCallbacks[addon]["CallPriority"][index] = nil;
 			E.callbacks:Fire(event);
 		end
 	end
@@ -528,7 +530,7 @@ function S:AddCallbackForAddon(addonName, eventName, loadFunc, forceLoad, bypass
 
 	--Create an event registry for this addon, so that we can fire multiple events when this addon is loaded
 	if(not self.addonCallbacks[addonName]) then
-		self.addonCallbacks[addonName] = {};
+		self.addonCallbacks[addonName] = {["CallPriority"] = {}};
 	end
 
 	if(self.addonCallbacks[addonName][eventName]) then
@@ -545,6 +547,7 @@ function S:AddCallbackForAddon(addonName, eventName, loadFunc, forceLoad, bypass
 	else
 		--Insert eventName in this addons' registry
 		self.addonCallbacks[addonName][eventName] = true;
+		self.addonCallbacks[addonName]["CallPriority"][#self.addonCallbacks[addonName]["CallPriority"] + 1] = eventName;
 	end
 end
 
@@ -567,6 +570,7 @@ function S:AddCallback(eventName, loadFunc)
 
 	--Add event name to registry
 	self.nonAddonCallbacks[eventName] = true;
+	self.nonAddonCallbacks["CallPriority"][#self.nonAddonCallbacks["CallPriority"] + 1] = eventName;
 
 	--Register loadFunc to be called when event is fired
 	E.RegisterCallback(E, eventName, loadFunc);
@@ -576,18 +580,20 @@ function S:Initialize()
 	self.db = E.private.skins;
 
 	--Fire events for Blizzard addons that are already loaded
-	for addon, events in pairs(self.addonCallbacks) do
+	for addon in pairs(self.addonCallbacks) do
 		if(IsAddOnLoaded(addon)) then
-			for event in pairs(events) do
+			for index, event in ipairs(self.addonCallbacks[addon]["CallPriority"]) do
 				self.addonCallbacks[addon][event] = nil;
+				self.addonCallbacks[addon]["CallPriority"][index] = nil;
 				E.callbacks:Fire(event);
 			end
 		end
 	end
 	--Fire event for all skins that doesn't rely on a Blizzard addon
-	for eventName in pairs(self.nonAddonCallbacks) do
-		self.addonCallbacks[eventName] = nil;
-		E.callbacks:Fire(eventName);
+	for index, event in ipairs(self.nonAddonCallbacks["CallPriority"]) do
+		self.nonAddonCallbacks[event] = nil;
+		self.nonAddonCallbacks["CallPriority"][index] = nil;
+		E.callbacks:Fire(event);
 	end
 
 	--Old deprecated load functions. We keep this for the time being in case plugins make use of it.
--- ElvUI/modules/unitframes/tags.lua
+++ ElvUI/modules/unitframes/tags.lua
@@ -172,8 +172,10 @@ end
 
 ElvUF.TagEvents["health:deficit-percent:name"] = "UNIT_HEALTH UNIT_MAXHEALTH";
 ElvUF.Tags["health:deficit-percent:name"] = function(unit)
+	local currentHealth = UnitHealth(unit)
 	local deficit = UnitHealthMax(unit) - UnitHealth(unit);
-	if(deficit > 0) then
+
+	if(deficit > 0 and currentHealth > 0) then
 		return _TAGS["health:percent-nostatus"](unit);
 	else
 		return _TAGS["name"](unit);
