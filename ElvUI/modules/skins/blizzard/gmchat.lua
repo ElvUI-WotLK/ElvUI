46fa3aa284f4718091e92dd047336ec66ea27738
Pinya <800pin.ru@gmail.com>
2016-12-09 16:31:21 +0300

    update GMChat skin

--- ElvUI/modules/skins/blizzard/gmchat.lua
+++ ElvUI/modules/skins/blizzard/gmchat.lua
@@ -4,11 +4,13 @@ local S = E:GetModule("Skins");
 local function LoadChatSkin()
 	if(not E.private.skins.blizzard.enable or not E.private.skins.blizzard.gmchat) then return; end
 
+	local LSM = LibStub("LibSharedMedia-3.0");
+
 	GMChatFrame:StripTextures();
 	GMChatFrame:CreateBackdrop("Transparent");
-	GMChatFrame.backdrop:Point("TOPLEFT", -2, 3);
+	GMChatFrame.backdrop:Point("TOPLEFT", -2, 6);
 	GMChatFrame.backdrop:Point("BOTTOMRIGHT", 2, -6);
-	GMChatFrame:SetClampRectInsets(-2, 2, 0, -10);
+	GMChatFrame:SetClampRectInsets(-6, 6, 33, -10);
 	GMChatFrame:Size(LeftChatPanel:GetWidth() - 4, 120);
 	GMChatFrame:Point("BOTTOMLEFT", LeftChatPanel, "TOPLEFT", 2, 5);
 	GMChatFrame:EnableMouseWheel(true);
@@ -16,11 +18,19 @@ local function LoadChatSkin()
 	GMChatTab:StripTextures();
 	GMChatTab:CreateBackdrop("Default");
 	GMChatTab.backdrop:Point("TOPLEFT", -2, 0);
-	GMChatTab.backdrop:Point("BOTTOMRIGHT", 2, -1);
+	GMChatTab.backdrop:Point("BOTTOMRIGHT", 2, 2);
+
+	GMChatTabText:Point("LEFT", GMChatTab, 17, 2);
+	GMChatTabText:FontTemplate(LSM:Fetch("font", E.db.chat.tabFont), E.db.chat.tabFontSize, E.db.chat.tabFontOutline);
+
+	if (not E.global.tukuiMode) then
+		GMChatTabText:SetTextColor(unpack(E["media"].rgbvaluecolor));
+	end
+
+	GMChatFrameCloseButton:Point("TOPRIGHT", GMChatTab, 6, 4);
+	S:HandleCloseButton(GMChatFrameCloseButton);
 
 	GMChatFrameButtonFrame:Kill();
-
-	S:HandleCloseButton(GMChatFrameCloseButton, GMChatTab);
 
 	local numScrollMessages = E.db.chat.numScrollMessages or 3
 	GMChatFrame:SetScript("OnMouseWheel", function(self, delta)
@@ -42,6 +52,31 @@ local function LoadChatSkin()
 			end
 		end
 	end);
+
+	local statusFrame = select(2, GMChatStatusFrame:GetChildren())
+	statusFrame:StripTextures();
+	statusFrame:CreateBackdrop("Transparent");
+	statusFrame.backdrop:Point("TOPLEFT", 0, 1);
+	statusFrame.backdrop:Point("BOTTOMRIGHT", 0, 0);
+
+	GMChatStatusFramePulse:SetTexture("Interface\\GMChatFrame\\UI-GMStatusFrame-Pulse")
+	GMChatStatusFramePulse:Point("TOPLEFT", -25, 21)
+	GMChatStatusFramePulse:Point("BOTTOMRIGHT", 25, -19)
+
+	GMChatStatusFrame:HookScript("OnShow", function(self)
+		if (TicketStatusFrame and TicketStatusFrame:IsShown()) then
+			self:Point("TOPLEFT", TicketStatusFrame, "BOTTOMLEFT", 0, 1);
+		else
+			self:SetAllPoints(TicketStatusFrame);
+		end
+	end)
+
+	TicketStatusFrame:HookScript("OnShow", function(self)
+		GMChatStatusFrame:Point("TOPLEFT", self, "BOTTOMLEFT", 0, 1);
+	end)
+	TicketStatusFrame:HookScript("OnHide", function(self)
+		GMChatStatusFrame:SetAllPoints(self);
+	end)
 end
 
 local function LoadSurveySkin()
