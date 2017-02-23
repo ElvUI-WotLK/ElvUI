be3be5bf7c2bc145745f445890e295b6ec0ac7e3
Pinya <800pin.ru@gmail.com>
2017-02-23 14:16:12 +0300

    update cache

--- ElvUI/modules/unitframes/elements/raidroleicons.lua
+++ ElvUI/modules/unitframes/elements/raidroleicons.lua
@@ -2,13 +2,20 @@ local E, L, V, P, G = unpack(select(2, ...));
 local UF = E:GetModule("UnitFrames");
 
 local match = string.match;
+local select, tonumber = select, tonumber
+
 local CreateFrame = CreateFrame;
+local GetNumRaidMembers = GetNumRaidMembers;
+local GetRaidRosterInfo = GetRaidRosterInfo;
+local IsPartyLeader = IsPartyLeader;
+local UnitInParty = UnitInParty;
+local UnitInRaid = UnitInRaid;
 
 local function CheckLeader(unit)
 	if(unit == "player") then
 		return IsPartyLeader();
 	elseif(unit ~= "player" and (UnitInParty(unit) or UnitInRaid(unit))) then
-		local gtype, index = unit:match("(%D+)(%d+)");
+		local gtype, index = match(unit, "(%D+)(%d+)");
 		index = tonumber(index);
 		if(gtype == "party" and GetNumRaidMembers() == 0) then
 			return GetPartyLeaderIndex() == index;
