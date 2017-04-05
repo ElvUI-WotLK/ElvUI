2b50ea40a5ac9c7db8416ff0f39476b128928ba2
Pinya <800pin.ru@gmail.com>
2017-04-05 20:25:04 +0300

    update Microbar mover size

--- ElvUI/modules/actionbars/microbar.lua
+++ ElvUI/modules/actionbars/microbar.lua
@@ -106,8 +106,10 @@ function AB:UpdateMicroPositionDimensions()
 		ElvUI_MicroBar:SetAlpha(self.db.microbar.alpha);
 	end
 
-	ElvUI_MicroBar:SetWidth(((CharacterMicroButton:GetWidth() - 4) * self.db.microbar.buttonsPerRow) + (self.db.microbar.xOffset * (self.db.microbar.buttonsPerRow-1)) + E.Border*2);
-	ElvUI_MicroBar:SetHeight(((CharacterMicroButton:GetHeight() - 28) * numRows) + (self.db.microbar.yOffset * (numRows-1)) + E.Border*2);
+	AB.MicroWidth = ((_G["CharacterMicroButton"]:GetWidth() - 4) * self.db.microbar.buttonsPerRow) + (self.db.microbar.xOffset * (self.db.microbar.buttonsPerRow - 1)) + E.Border * 2
+	AB.MicroHeight = ((_G["CharacterMicroButton"]:GetHeight() - 28) * numRows) + (self.db.microbar.yOffset * (numRows - 1)) + E.Border * 2
+ 	ElvUI_MicroBar:Width(AB.MicroWidth)
+ 	ElvUI_MicroBar:Height(AB.MicroHeight)
 
 	if(self.db.microbar.enabled) then
 		ElvUI_MicroBar:Show();
