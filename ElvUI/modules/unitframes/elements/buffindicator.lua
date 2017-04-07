b0c0eb4dca81875bfd130d38cb009907c4e8ecd5
Bunny67 <zolov67@yandex.ru>
2017-04-07 20:19:41 +0300

    fix #386

--- ElvUI/modules/unitframes/elements/buffindicator.lua
+++ ElvUI/modules/unitframes/elements/buffindicator.lua
@@ -155,6 +155,8 @@ function UF:UpdateAuraWatch(frame, petOverride, db)
 				if(not icon.cd) then
 					icon.cd = CreateFrame("Cooldown", nil, icon, "CooldownFrameTemplate");
 					icon.cd:SetAllPoints(icon);
+					icon.cd.noOCC = true;
+					icon.cd.noCooldownCount = true;
 					icon.cd:SetReverse(true);
 					icon.cd:SetFrameLevel(icon:GetFrameLevel());
 				end
