71586a21d02cd37ecc97cf7543bc4350f971640b
Pinya <800pin.ru@gmail.com>
2017-07-08 14:11:59 +0300

    added global key to determine state of AFK module
    also able to determinet if logout was while AFk wwas enabled

--- ElvUI/modules/misc/afk.lua
+++ ElvUI/modules/misc/afk.lua
@@ -84,6 +84,8 @@ function AFK:SetAFK(status)
 		UIParent:Hide();
 		self.AFKMode:Show();
 
+		E.global.afkEnabled = true
+
 		MoveViewLeftStart(CAMERA_SPEED);
 
 		if(IsInGuild()) then
@@ -113,6 +115,8 @@ function AFK:SetAFK(status)
 	elseif(not status and self.isAFK) then
 		self.AFKMode:Hide();
 		UIParent:Show();
+
+		E.global.afkEnabled = nil
 
 		MoveViewLeftStop();
 
@@ -240,6 +244,10 @@ end
 ]]
 
 function AFK:Initialize()
+	if E.global.afkEnabled then
+		E.global.afkEnabled = nil
+	end
+
 	local classColor = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[E.myclass] or RAID_CLASS_COLORS[E.myclass];
 
 	self.AFKMode = CreateFrame("Frame", "ElvUIAFKFrame");
