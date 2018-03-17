587d804c2b00a801f6b7f1984631eeae2cdb0b86
Bunny67 <zolov67@yandex.ru>
2018-03-17 22:39:21 +0300

    update time datatext

--- ElvUI/modules/datatexts/time.lua
+++ ElvUI/modules/datatexts/time.lua
@@ -1,8 +1,10 @@
 local E, L, V, P, G = unpack(select(2, ...));
 local DT = E:GetModule("DataTexts");
 
-local time = time;
-local format, join = string.format, string.join;
+local next, unpack = next, unpack
+local format, join = string.format, string.join
+local tinsert = table.insert
+local time, utf8sub = time, string.utf8sub
 
 local GetGameTime = GetGameTime;
 local GetNumSavedInstances = GetNumSavedInstances;
@@ -17,7 +19,7 @@ local WINTERGRASP_IN_PROGRESS = WINTERGRASP_IN_PROGRESS;
 local timeDisplayFormat = "";
 local dateDisplayFormat = "";
 local europeDisplayFormat_nocolor = join("", "%02d", ":|r%02d");
-local lockoutInfoFormatNoEnc = "%s%s |cffaaaaaa(%s)";
+local lockoutInfoFormat = "%s%s %s |cffaaaaaa(%s)"
 local difficultyInfo = {"N", "N", "H", "H"};
 local lockoutColorExtended, lockoutColorNormal = {r = 0.3, g = 1, b = 0.3}, {r = .8, g = .8, b = .8};
 
@@ -34,8 +36,41 @@ local function OnLeave()
 	DT.tooltip:Hide();
 end
 
+local instanceIconByName = {}
+local function GetInstanceImages(...)
+	local numTextures = select("#", ...) / 4
+
+	local param, title, texture = 1
+	for textureIndex = 1, numTextures do
+		title = select(param, ...)
+		param = param + 1
+		texture = select(param, ...)
+		param = param + 1
+		instanceIconByName[title] = texture
+		param = param + 2
+	end
+end
+
+local locale = GetLocale()
+local krcntw = locale == "koKR" or locale == "zhCN" or locale == "zhTW"
+local difficultyTag = { -- Normal, Heroic, Normal, Heroic
+	(krcntw and PLAYER_DIFFICULTY1) or utf8sub(PLAYER_DIFFICULTY1, 1, 1), -- N
+	(krcntw and PLAYER_DIFFICULTY2) or utf8sub(PLAYER_DIFFICULTY2, 1, 1), -- H
+	(krcntw and PLAYER_DIFFICULTY1) or utf8sub(PLAYER_DIFFICULTY1, 1, 1), -- N
+	(krcntw and PLAYER_DIFFICULTY2) or utf8sub(PLAYER_DIFFICULTY2, 1, 1), -- H
+}
+
+local collectedInstanceImages = false
 local function OnEnter(self)
 	DT:SetupTooltip(self)
+
+	RequestRaidInfo()
+
+	if not collectedInstanceImages then
+		GetInstanceImages(CalendarEventGetTextures(1))
+		GetInstanceImages(CalendarEventGetTextures(2))
+		collectedInstanceImages = true
+	end
 
 	local wgtime = GetWintergraspWaitTime() or nil;
 	local _, instanceType = IsInInstance();
@@ -46,25 +81,50 @@ local function OnEnter(self)
 	else
 		wgtime = SecondsToTime(wgtime, false, nil, 3);
 	end
+
 	DT.tooltip:AddDoubleLine(L["Wintergrasp"], wgtime, 1, 1, 1, lockoutColorNormal.r, lockoutColorNormal.g, lockoutColorNormal.b);
 
-	local name, _, reset, difficultyId, locked, extended, isRaid, maxPlayers;
-	local oneraid, lockoutColor
+	local lockedInstances = {raids = {}, dungeons = {}}
+	local name, reset, difficulty, locked, extended, isRaid, maxPlayers, difficultyName
+	local isHeroicDungeon, difficultyLetter, buttonImg
+	
 	for i = 1, GetNumSavedInstances() do
-		name, _, reset, difficultyId, locked, extended, _, isRaid, maxPlayers = GetSavedInstanceInfo(i);
-		if(isRaid and (locked or extended) and name) then
-			if(not oneraid) then
-				DT.tooltip:AddLine(" ");
-				DT.tooltip:AddLine(L["Saved Raid(s)"]);
-				oneraid = true;
-			end
-			if(extended) then
-				lockoutColor = lockoutColorExtended;
-			else
-				lockoutColor = lockoutColorNormal;
-			end
+		name, _, reset, difficulty, locked, extended, _, isRaid, maxPlayers, difficultyName = GetSavedInstanceInfo(i);
+		if (locked or extended) and name then
+			isHeroicDungeon = difficulty == 2
 
-			DT.tooltip:AddDoubleLine(format(lockoutInfoFormatNoEnc, maxPlayers, difficultyInfo[difficultyId], name), SecondsToTime(reset, false, nil, 3), 1, 1, 1, lockoutColor.r, lockoutColor.g, lockoutColor.b);
+			difficultyLetter = difficultyTag[difficulty]
+			buttonImg = instanceIconByName[name] and format("|T%s:22:22:0:0:96:96:0:64:0:64|t ", "Interface\\LFGFrame\\LFGIcon-"..instanceIconByName[name]) or ""
+
+			if isRaid then
+				tinsert(lockedInstances["raids"], {name, buttonImg, reset, difficultyLetter, extended, maxPlayers})
+			elseif isHeroicDungeon then
+				tinsert(lockedInstances["dungeons"], {name, buttonImg, reset, difficultyLetter, extended, maxPlayers})
+			end
+		end
+	end
+
+	if next(lockedInstances["raids"]) then
+		DT.tooltip:AddLine(" ")
+		DT.tooltip:AddLine(L["Saved Raid(s)"])
+
+		for i = 1, #lockedInstances["raids"] do
+			name, buttonImg, reset, difficultyLetter, extended, maxPlayers = unpack(lockedInstances["raids"][i])
+
+			lockoutColor = extended and lockoutColorExtended or lockoutColorNormal
+			DT.tooltip:AddDoubleLine(format(lockoutInfoFormat, buttonImg, maxPlayers, difficultyLetter, name), SecondsToTime(reset, false, nil, 3), 1, 1, 1, lockoutColor.r, lockoutColor.g, lockoutColor.b)
+		end
+	end
+
+	if next(lockedInstances["dungeons"]) then
+		DT.tooltip:AddLine(" ")
+		DT.tooltip:AddLine(L["Saved Dungeon(s)"])
+
+		for i = 1, #lockedInstances["dungeons"] do
+			name, buttonImg, reset, difficultyLetter, extended, maxPlayers = unpack(lockedInstances["dungeons"][i])
+
+			lockoutColor = extended and lockoutColorExtended or lockoutColorNormal
+			DT.tooltip:AddDoubleLine(format(lockoutInfoFormat, buttonImg, maxPlayers, difficultyLetter, name), SecondsToTime(reset, false, nil, 3), 1, 1, 1, lockoutColor.r, lockoutColor.g, lockoutColor.b)
 		end
 	end
 
