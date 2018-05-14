7688d107fe5d4d3bca6c77bfdcad1e5640b67e5e
Apollyonn <jellefenxd@gmail.com>
2018-05-14 17:52:26 +0300

    update

--- ElvUI/modules/misc/misc.lua
+++ ElvUI/modules/misc/misc.lua
@@ -43,18 +43,13 @@ function M:COMBAT_LOG_EVENT_UNFILTERED(_, _, event, sourceGUID, _, _, _, destNam
 	if(E.db.general.interruptAnnounce == "NONE") then return; end
 	if not (event == "SPELL_INTERRUPT" and (sourceGUID == E.myguid or sourceGUID == UnitGUID("pet"))) then return; end
 
-	local party = GetNumPartyMembers();
 
 	if(E.db.general.interruptAnnounce == "SAY") then
-		if(party > 0) then
-			SendChatMessage(format(interruptMsg, destName, spellID, spellName), "SAY");
-		end
+		SendChatMessage(format(interruptMsg, destName, spellID, spellName), "SAY");
 	elseif E.db.general.interruptAnnounce == "EMOTE" then
-		if(party > 0) then
-			SendChatMessage(format(interruptMsg, destName, spellID, spellName), "EMOTE")
-		end
+		SendChatMessage(format(interruptMsg, destName, spellID, spellName), "EMOTE")
 	else
-		local raid = GetNumRaidMembers();
+		local party, raid = GetNumPartyMembers(), GetNumRaidMembers()
 		local _, instanceType = IsInInstance();
 		local battleground = instanceType == "pvp";
 
