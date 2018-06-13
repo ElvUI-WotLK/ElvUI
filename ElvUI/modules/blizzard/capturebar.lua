7d2e0ebd6de8f432355477b7f861ecd062cba3fd
Apollyonn <jellefenxd@gmail.com>
2018-06-13 14:27:25 +0300

    update

--- ElvUI/modules/blizzard/capturebar.lua
+++ ElvUI/modules/blizzard/capturebar.lua
@@ -9,18 +9,35 @@ function B:WorldStateAlwaysUpFrame_Update()
 	local captureBar
 	for i = 1, NUM_EXTENDED_UI_FRAMES do
 		captureBar = _G["WorldStateCaptureBar"..i]
+
 		if captureBar and captureBar:IsShown() then
 			captureBar:ClearAllPoints()
-			captureBar:Point("TOP", pvpHolder, "BOTTOM", 0, -50)
+			captureBar:Point("TOP", pvpHolder, "BOTTOM", 0, -75)
+			captureBar.SetPoint = E.noop
 		end
 	end
 
-	if AlwaysUpFrame1 then
-		AlwaysUpFrame1:ClearAllPoints()
-		AlwaysUpFrame1:Point("CENTER", pvpHolder, "CENTER", -25, 0)
-	end
-	if AlwaysUpFrame2 then
-		AlwaysUpFrame2:Point("TOP", AlwaysUpFrame1, "BOTTOM", 0, -5)
+	for i = 1, NUM_ALWAYS_UP_UI_FRAMES do
+		local frame = _G["AlwaysUpFrame"..i]
+		local text = _G["AlwaysUpFrame"..i.."Text"]
+		local icon = _G["AlwaysUpFrame"..i.."Icon"]
+		local dynamic = _G["AlwaysUpFrame"..i.."DynamicIconButton"]
+
+		if frame then
+			if i == 1 then
+				frame:ClearAllPoints()
+				frame:Point("CENTER", pvpHolder, "CENTER", 0, 5)
+			end
+
+			text:ClearAllPoints()
+			text:Point("CENTER", frame, "CENTER", 0, 0)
+
+			icon:ClearAllPoints()
+			icon:Point("CENTER", text, "LEFT", -10, -9)
+
+			dynamic:ClearAllPoints()
+			dynamic:Point("LEFT", text, "RIGHT", 5, 0)
+		end
 	end
 end
 
