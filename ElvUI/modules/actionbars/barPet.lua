88948f5d1dc17e4446c75ccfc11f4967a0cc8ff2
Apollyonn <jellefenxd@gmail.com>
2018-06-20 16:58:40 +0300

    Fix #533

--- ElvUI/modules/actionbars/barPet.lua
+++ ElvUI/modules/actionbars/barPet.lua
@@ -1,230 +1,247 @@
-local E, L, V, P, G = unpack(select(2, ...)); -- Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
-local AB = E:GetModule("ActionBars");
+local E, L, V, P, G = unpack(select(2, ...))
+local AB = E:GetModule("ActionBars")
 
-local _G = _G;
-local ceil = math.ceil;
+local _G = _G
+local ceil = math.ceil
 
-local RegisterStateDriver = RegisterStateDriver;
-local GetBindingKey = GetBindingKey;
-local PetHasActionBar = PetHasActionBar;
-local GetPetActionInfo = GetPetActionInfo;
-local IsPetAttackAction = IsPetAttackAction;
-local PetActionButton_StartFlash = PetActionButton_StartFlash;
-local PetActionButton_StopFlash = PetActionButton_StopFlash;
-local AutoCastShine_AutoCastStart = AutoCastShine_AutoCastStart;
-local AutoCastShine_AutoCastStop = AutoCastShine_AutoCastStop;
-local GetPetActionSlotUsable = GetPetActionSlotUsable;
-local SetDesaturation = SetDesaturation;
-local PetActionBar_ShowGrid = PetActionBar_ShowGrid;
-local PetActionBar_UpdateCooldowns = PetActionBar_UpdateCooldowns;
-local NUM_PET_ACTION_SLOTS = NUM_PET_ACTION_SLOTS;
+local RegisterStateDriver = RegisterStateDriver
+local GetBindingKey = GetBindingKey
+local PetHasActionBar = PetHasActionBar
+local GetPetActionInfo = GetPetActionInfo
+local IsPetAttackAction = IsPetAttackAction
+local PetActionButton_StartFlash = PetActionButton_StartFlash
+local PetActionButton_StopFlash = PetActionButton_StopFlash
+local AutoCastShine_AutoCastStart = AutoCastShine_AutoCastStart
+local AutoCastShine_AutoCastStop = AutoCastShine_AutoCastStop
+local GetPetActionSlotUsable = GetPetActionSlotUsable
+local SetDesaturation = SetDesaturation
+local PetActionBar_ShowGrid = PetActionBar_ShowGrid
+local PetActionBar_UpdateCooldowns = PetActionBar_UpdateCooldowns
+local NUM_PET_ACTION_SLOTS = NUM_PET_ACTION_SLOTS
 
-local bar = CreateFrame("Frame", "ElvUI_BarPet", E.UIParent, "SecureHandlerStateTemplate");
-bar:SetFrameStrata("LOW");
+local bar = CreateFrame("Frame", "ElvUI_BarPet", E.UIParent, "SecureHandlerStateTemplate")
+bar:SetFrameStrata("LOW")
 
 function AB:UpdatePet(event, unit)
-	if ((event == "UNIT_FLAGS" or event == "UNIT_AURA") and unit ~= "pet") then return; end
-	if (event == "UNIT_PET" and unit ~= "player") then return; end
+	if (event == "UNIT_FLAGS" or event == "UNIT_AURA") and unit ~= "pet" then return end
+	if event == "UNIT_PET" and unit ~= "player" then return end
 
-	local petActionButton, petActionIcon, petAutoCastableTexture, petAutoCastShine;
 	for i = 1, NUM_PET_ACTION_SLOTS, 1 do
-		local buttonName = "PetActionButton" .. i;
-		petActionButton = _G[buttonName];
-		petActionIcon = _G[buttonName .. "Icon"];
-		petAutoCastableTexture = _G[buttonName .. "AutoCastable"];
-		petAutoCastShine = _G[buttonName .. "Shine"];
-		local name, subtext, texture, isToken, isActive, autoCastAllowed, autoCastEnabled = GetPetActionInfo(i);
-		if(not isToken) then
-			petActionIcon:SetTexture(texture);
-			petActionButton.tooltipName = name;
+		local buttonName = "PetActionButton"..i
+		local button = _G[buttonName]
+		local icon = _G[buttonName.."Icon"]
+		local autoCast = _G[buttonName.."AutoCastable"]
+		local shine = _G[buttonName.."Shine"]
+		local name, subtext, texture, isToken, isActive, autoCastAllowed, autoCastEnabled = GetPetActionInfo(i)
+
+		if not isToken then
+			icon:SetTexture(texture)
+			button.tooltipName = name
 		else
-			petActionIcon:SetTexture(_G[texture]);
-			petActionButton.tooltipName = _G[name];
+			icon:SetTexture(_G[texture])
+			button.tooltipName = _G[name]
 		end
-		petActionButton.isToken = isToken;
-		petActionButton.tooltipSubtext = subtext;
-		if(isActive and name ~= "PET_ACTION_FOLLOW") then
-			petActionButton:SetChecked(true);
-			if(IsPetAttackAction(i)) then
-				PetActionButton_StartFlash(petActionButton);
+
+		button.isToken = isToken
+		button.tooltipSubtext = subtext
+
+		if isActive and name ~= "PET_ACTION_FOLLOW" then
+			button:SetChecked(true)
+
+			if IsPetAttackAction(i) then
+				PetActionButton_StartFlash(button)
 			end
 		else
-			petActionButton:SetChecked(false);
-			if(IsPetAttackAction(i)) then
-				PetActionButton_StopFlash(petActionButton);
+			button:SetChecked(false)
+			if IsPetAttackAction(i) then
+				PetActionButton_StopFlash(button)
 			end
 		end
-		if(autoCastAllowed) then
-			petAutoCastableTexture:Show();
+
+		if autoCastAllowed then
+			autoCast:Show()
 		else
-			petAutoCastableTexture:Hide();
+			autoCast:Hide()
 		end
-		if(autoCastEnabled) then
-			AutoCastShine_AutoCastStart(petAutoCastShine);
+
+		if autoCastEnabled then
+			AutoCastShine_AutoCastStart(shine)
 		else
-			AutoCastShine_AutoCastStop(petAutoCastShine);
+			AutoCastShine_AutoCastStop(shine)
 		end
-		if(texture) then
-			if(GetPetActionSlotUsable(i)) then
-				SetDesaturation(petActionIcon, nil);
+
+		button:SetAlpha(1)
+
+		if texture then
+			if GetPetActionSlotUsable(i) then
+				SetDesaturation(icon, nil)
 			else
-				SetDesaturation(petActionIcon, 1);
+				SetDesaturation(icon, 1)
 			end
-			petActionIcon:Show();
+			icon:Show()
 		else
-			petActionIcon:Hide();
+			icon:Hide()
 		end
-		if(not PetHasActionBar() and texture and name ~= "PET_ACTION_FOLLOW") then
-			PetActionButton_StopFlash(petActionButton);
-			SetDesaturation(petActionIcon, 1);
-			petActionButton:SetChecked(false);
+
+		if not PetHasActionBar() and texture and name ~= "PET_ACTION_FOLLOW" then
+			PetActionButton_StopFlash(button)
+			SetDesaturation(icon, 1)
+			button:SetChecked(0)
 		end
 	end
 end
 
 function AB:PositionAndSizeBarPet()
-	local buttonSpacing = E:Scale(self.db["barPet"].buttonspacing);
-	local backdropSpacing = E:Scale((self.db["barPet"].backdropSpacing or self.db["barPet"].buttonspacing));
-	local buttonsPerRow = self.db["barPet"].buttonsPerRow;
-	local numButtons = self.db["barPet"].buttons;
-	local size = E:Scale(self.db["barPet"].buttonsize);
+	local buttonSpacing = E:Scale(self.db["barPet"].buttonspacing)
+	local backdropSpacing = E:Scale((self.db["barPet"].backdropSpacing or self.db["barPet"].buttonspacing))
+	local buttonsPerRow = self.db["barPet"].buttonsPerRow
+	local numButtons = self.db["barPet"].buttons
+	local size = E:Scale(self.db["barPet"].buttonsize)
 	local autoCastSize = (size / 2) - (size / 7.5)
-	local point = self.db["barPet"].point;
-	local numColumns = ceil(numButtons / buttonsPerRow);
-	local widthMult = self.db["barPet"].widthMult;
-	local heightMult = self.db["barPet"].heightMult;
+	local point = self.db["barPet"].point
+	local numColumns = ceil(numButtons / buttonsPerRow)
+	local widthMult = self.db["barPet"].widthMult
+	local heightMult = self.db["barPet"].heightMult
+	local visibility = self.db["barPet"].visibility
+
+	bar.db = self.db["barPet"]
+	bar.db.position = nil --Depreciated
+
+	if visibility and visibility:match("[\n\r]") then
+		visibility = visibility:gsub("[\n\r]","")
+	end
 
 	if numButtons < buttonsPerRow then
-		buttonsPerRow = numButtons;
+		buttonsPerRow = numButtons
 	end
 
 	if numColumns < 1 then
-		numColumns = 1;
+		numColumns = 1
 	end
 
 	if self.db["barPet"].backdrop == true then
-		bar.backdrop:Show();
+		bar.backdrop:Show()
 	else
-		bar.backdrop:Hide();
-
+		bar.backdrop:Hide()
+		--Set size multipliers to 1 when backdrop is disabled
 		widthMult = 1
 		heightMult = 1
 	end
 
 	local barWidth = (size * (buttonsPerRow * widthMult)) + ((buttonSpacing * (buttonsPerRow - 1)) * widthMult) + (buttonSpacing * (widthMult-1)) + ((self.db["barPet"].backdrop == true and (E.Border + backdropSpacing) or E.Spacing)*2)
 	local barHeight = (size * (numColumns * heightMult)) + ((buttonSpacing * (numColumns - 1)) * heightMult) + (buttonSpacing * (heightMult-1)) + ((self.db["barPet"].backdrop == true and (E.Border + backdropSpacing) or E.Spacing)*2)
-	bar:Width(barWidth);
-	bar:Height(barHeight);
-
-	bar.mover:SetSize(bar:GetSize());
+	bar:Width(barWidth)
+	bar:Height(barHeight)
 
 	if self.db["barPet"].enabled then
-		bar:SetScale(1);
-		bar:SetAlpha(self.db["barPet"].alpha);
-		E:EnableMover(bar.mover:GetName());
+		bar:SetScale(1)
+		bar:SetAlpha(bar.db.alpha)
+		E:EnableMover(bar.mover:GetName())
 	else
-		bar:SetScale(0.0001);
-		bar:SetAlpha(0);
-		E:DisableMover(bar.mover:GetName());
+		bar:SetScale(0.0001)
+		bar:SetAlpha(0)
+		E:DisableMover(bar.mover:GetName())
 	end
 
-	local horizontalGrowth, verticalGrowth;
+	local horizontalGrowth, verticalGrowth
 	if point == "TOPLEFT" or point == "TOPRIGHT" then
-		verticalGrowth = "DOWN";
+		verticalGrowth = "DOWN"
 	else
-		verticalGrowth = "UP";
+		verticalGrowth = "UP"
 	end
 
 	if point == "BOTTOMLEFT" or point == "TOPLEFT" then
-		horizontalGrowth = "RIGHT";
+		horizontalGrowth = "RIGHT"
 	else
-		horizontalGrowth = "LEFT";
+		horizontalGrowth = "LEFT"
 	end
 
-	bar.mouseover = self.db["barPet"].mouseover;
-	if(bar.mouseover) then
-		bar:SetAlpha(0);
+	bar.mouseover = self.db["barPet"].mouseover
+	if bar.mouseover then
+		bar:SetAlpha(0)
 	else
-		bar:SetAlpha(self.db["barPet"].alpha);
+		bar:SetAlpha(bar.db.alpha)
 	end
 
-	if(self.db["barPet"].inheritGlobalFade) then
-		bar:SetParent(self.fadeParent);
+	if self.db["barPet"].inheritGlobalFade then
+		bar:SetParent(self.fadeParent)
 	else
-		bar:SetParent(E.UIParent);
+		bar:SetParent(E.UIParent)
 	end
 
-	local button, lastButton, lastColumnButton, autoCast, shine;
-	local firstButtonSpacing = (self.db["barPet"].backdrop == true and (E.Border + backdropSpacing) or E.Spacing);
+	local button, lastButton, lastColumnButton, autoCast, shine
+	local firstButtonSpacing = (self.db["barPet"].backdrop == true and (E.Border + backdropSpacing) or E.Spacing)
 	for i = 1, NUM_PET_ACTION_SLOTS do
-		button = _G["PetActionButton"..i];
-		lastButton = _G["PetActionButton"..i-1];
-		autoCast = _G["PetActionButton"..i.."AutoCastable"];
+		button = _G["PetActionButton"..i]
+		lastButton = _G["PetActionButton"..i - 1]
+		autoCast = _G["PetActionButton"..i.."AutoCastable"]
 		shine = _G["PetActionButton"..i.."Shine"]
-		lastColumnButton = _G["PetActionButton"..i-buttonsPerRow];
-		button:SetParent(bar);
-		button:ClearAllPoints();
-		button:Size(size);
+		lastColumnButton = _G["PetActionButton"..i-buttonsPerRow]
+
+		button:SetParent(bar)
+		button:ClearAllPoints()
+		button:Size(size)
 		autoCast:SetOutside(button, autoCastSize, autoCastSize)
 		shine:SetInside(button)
-		button:SetAttribute("showgrid", 1);
+		button:SetAttribute("showgrid", 1)
 
-		if(i == 1) then
-			local x, y;
-			if(point == "BOTTOMLEFT") then
-				x, y = firstButtonSpacing, firstButtonSpacing;
-			elseif(point == "TOPRIGHT") then
-				x, y = -firstButtonSpacing, -firstButtonSpacing;
-			elseif(point == "TOPLEFT") then
-				x, y = firstButtonSpacing, -firstButtonSpacing;
+		if i == 1 then
+			local x, y
+			if point == "BOTTOMLEFT" then
+				x, y = firstButtonSpacing, firstButtonSpacing
+			elseif point == "TOPRIGHT" then
+				x, y = -firstButtonSpacing, -firstButtonSpacing
+			elseif point == "TOPLEFT" then
+				x, y = firstButtonSpacing, -firstButtonSpacing
 			else
-				x, y = -firstButtonSpacing, firstButtonSpacing;
+				x, y = -firstButtonSpacing, firstButtonSpacing
 			end
 
-			button:Point(point, bar, point, x, y);
-		elseif((i - 1) % buttonsPerRow == 0) then
-			local x = 0;
-			local y = -buttonSpacing;
-			local buttonPoint, anchorPoint = "TOP", "BOTTOM";
-			if(verticalGrowth == "UP") then
-				y = buttonSpacing;
-				buttonPoint = "BOTTOM";
-				anchorPoint = "TOP";
+			button:Point(point, bar, point, x, y)
+		elseif (i - 1) % buttonsPerRow == 0 then
+			local x = 0
+			local y = -buttonSpacing
+			local buttonPoint, anchorPoint = "TOP", "BOTTOM"
+			if verticalGrowth == "UP" then
+				y = buttonSpacing
+				buttonPoint = "BOTTOM"
+				anchorPoint = "TOP"
 			end
-			button:Point(buttonPoint, lastColumnButton, anchorPoint, x, y);
+			button:Point(buttonPoint, lastColumnButton, anchorPoint, x, y)
 		else
-			local x = buttonSpacing;
-			local y = 0;
-			local buttonPoint, anchorPoint = "LEFT", "RIGHT";
-			if(horizontalGrowth == "LEFT") then
-				x = -buttonSpacing;
-				buttonPoint = "RIGHT";
-				anchorPoint = "LEFT";
+			local x = buttonSpacing
+			local y = 0
+			local buttonPoint, anchorPoint = "LEFT", "RIGHT"
+			if horizontalGrowth == "LEFT" then
+				x = -buttonSpacing
+				buttonPoint = "RIGHT"
+				anchorPoint = "LEFT"
 			end
 
-			button:Point(buttonPoint, lastButton, anchorPoint, x, y);
+			button:Point(buttonPoint, lastButton, anchorPoint, x, y)
 		end
 
 		if i > numButtons then
-			button:SetScale(0.0001);
-			button:SetAlpha(0);
+			button:SetScale(0.0001)
+			button:SetAlpha(0)
 		else
-			button:SetScale(1);
-			button:SetAlpha(1);
+			button:SetScale(1)
+			button:SetAlpha(bar.db.alpha)
 		end
 
-		self:StyleButton(button, nil, self.LBFGroup and E.private.actionbar.lbf.enable and true or nil);
+		self:StyleButton(button, nil, self.LBFGroup and E.private.actionbar.lbf.enable and true or nil)
 	end
 
-	RegisterStateDriver(bar, "visibility", self.db["barPet"].visibility);
+	RegisterStateDriver(bar, "show", visibility)
 
+	--Fix issue with mover not updating size when bar is hidden
 	bar:GetScript("OnSizeChanged")(bar)
 
-	if(self.LBFGroup and E.private.actionbar.lbf.enable) then self.LBFGroup:Skin(E.private.actionbar.lbf.skin); end
+	if self.LBFGroup and E.private.actionbar.lbf.enable then self.LBFGroup:Skin(E.private.actionbar.lbf.skin) end
 end
 
 function AB:UpdatePetBindings()
-	for i=1, NUM_PET_ACTION_SLOTS do
+	for i = 1, NUM_PET_ACTION_SLOTS do
 		if self.db.hotkeytext then
 			local key = GetBindingKey("BONUSACTIONBUTTON"..i)
 			_G["PetActionButton"..i.."HotKey"]:Show()
@@ -237,12 +254,12 @@ function AB:UpdatePetBindings()
 end
 
 function AB:CreateBarPet()
-	bar:CreateBackdrop("Default");
-	bar.backdrop:SetAllPoints();
+	bar:CreateBackdrop("Default")
+	bar.backdrop:SetAllPoints()
 	if self.db["bar4"].enabled then
-		bar:Point("RIGHT", ElvUI_Bar4, "LEFT", -4, 0);
+		bar:Point("RIGHT", ElvUI_Bar4, "LEFT", -4, 0)
 	else
-		bar:Point("RIGHT", E.UIParent, "RIGHT", -4, 0);
+		bar:Point("RIGHT", E.UIParent, "RIGHT", -4, 0)
 	end
 
 	bar:SetAttribute("_onstate-show", [[
@@ -251,38 +268,38 @@ function AB:CreateBarPet()
 		else
 			self:Show();
 		end
-	]]);
+	]])
 
-	PetActionBarFrame.showgrid = 1;
-	PetActionBar_ShowGrid();
+	PetActionBarFrame.showgrid = 1
+	PetActionBar_ShowGrid()
 
-	self:HookScript(bar, "OnEnter", "Bar_OnEnter");
-	self:HookScript(bar, "OnLeave", "Bar_OnLeave");
+	self:HookScript(bar, "OnEnter", "Bar_OnEnter")
+	self:HookScript(bar, "OnLeave", "Bar_OnLeave")
 	for i = 1, NUM_PET_ACTION_SLOTS do
-		self:HookScript(_G["PetActionButton" .. i], "OnEnter", "Button_OnEnter");
-		self:HookScript(_G["PetActionButton" .. i], "OnLeave", "Button_OnLeave");
+		self:HookScript(_G["PetActionButton" .. i], "OnEnter", "Button_OnEnter")
+		self:HookScript(_G["PetActionButton" .. i], "OnLeave", "Button_OnLeave")
 	end
 
-	self:RegisterEvent("SPELLS_CHANGED", "UpdatePet");
-	self:RegisterEvent("PLAYER_CONTROL_GAINED", "UpdatePet");
-	self:RegisterEvent("PLAYER_ENTERING_WORLD", "UpdatePet");
-	self:RegisterEvent("PLAYER_CONTROL_LOST", "UpdatePet");
-	self:RegisterEvent("PET_BAR_UPDATE", "UpdatePet");
-	self:RegisterEvent("UNIT_PET", "UpdatePet");
-	self:RegisterEvent("UNIT_FLAGS", "UpdatePet");
-	self:RegisterEvent("UNIT_AURA", "UpdatePet");
-	self:RegisterEvent("PLAYER_FARSIGHT_FOCUS_CHANGED", "UpdatePet");
-	self:RegisterEvent("PET_BAR_UPDATE_COOLDOWN", PetActionBar_UpdateCooldowns);
+	self:RegisterEvent("SPELLS_CHANGED", "UpdatePet")
+	self:RegisterEvent("PLAYER_CONTROL_GAINED", "UpdatePet")
+	self:RegisterEvent("PLAYER_ENTERING_WORLD", "UpdatePet")
+	self:RegisterEvent("PLAYER_CONTROL_LOST", "UpdatePet")
+	self:RegisterEvent("PET_BAR_UPDATE", "UpdatePet")
+	self:RegisterEvent("UNIT_PET", "UpdatePet")
+	self:RegisterEvent("UNIT_FLAGS", "UpdatePet")
+	self:RegisterEvent("UNIT_AURA", "UpdatePet")
+	self:RegisterEvent("PLAYER_FARSIGHT_FOCUS_CHANGED", "UpdatePet")
+	self:RegisterEvent("PET_BAR_UPDATE_COOLDOWN", PetActionBar_UpdateCooldowns)
 
-	E:CreateMover(bar, "ElvBar_Pet", L["Pet Bar"], nil, nil, nil,"ALL,ACTIONBARS");
+	E:CreateMover(bar, "ElvBar_Pet", L["Pet Bar"], nil, nil, nil,"ALL,ACTIONBARS")
 
-	self:PositionAndSizeBarPet();
-	self:UpdatePetBindings();
+	self:PositionAndSizeBarPet()
+	self:UpdatePetBindings()
 
-	if(self.LBFGroup and E.private.actionbar.lbf.enable) then
+	if self.LBFGroup and E.private.actionbar.lbf.enable then
 		for i = 1, NUM_PET_ACTION_SLOTS do
-			local button = _G["PetActionButton" .. i];
-			self.LBFGroup:AddButton(button);
+			local button = _G["PetActionButton"..i]
+			self.LBFGroup:AddButton(button)
 		end
 	end
 end
