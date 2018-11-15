7f8dd7b6991bd1386e04db22255c48d3c5f0ff17
Nyll <nyll@sirus.su>
2018-11-15 17:32:14 +0300

    add check for color close #8, #9

--- ElvUI/modules/tooltip/tooltip.lua
+++ ElvUI/modules/tooltip/tooltip.lua
@@ -419,7 +419,10 @@ function TT:GameTooltip_OnTooltipSetUnit(tt)
 			local itemLevel = ItemLevelMixIn:GetItemLevel( unitGUID )
 			if itemLevel then
 				local color = ItemLevelMixIn:GetColor(itemLevel)
-				tooltipLine = string.gsub(TOOLTIP_UNIT_LEVEL_ILEVEL_LABEL, TOOLTIP_UNIT_LEVEL_ILEVEL_LOADING_LABEL, color:WrapTextInColorCode(itemLevel))
+				
+				if color then
+					tooltipLine = string.gsub(TOOLTIP_UNIT_LEVEL_ILEVEL_LABEL, TOOLTIP_UNIT_LEVEL_ILEVEL_LOADING_LABEL, color:WrapTextInColorCode(itemLevel))
+				end
 			end
 		end
 
