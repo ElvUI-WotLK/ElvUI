34d257d1ab6f3925120cfb2664a78cac607dd98d
Apollyonn <jellefenxd@gmail.com>
2019-08-11 17:21:43 +0300

    update

--- ElvUI/Modules/UnitFrames/ConfigEnviroment.lua
+++ ElvUI/Modules/UnitFrames/ConfigEnviroment.lua
@@ -1,27 +1,27 @@
 local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
-local UF = E:GetModule("UnitFrames");
+local UF = E:GetModule("UnitFrames")
 local _, ns = ...
 local ElvUF = ns.oUF
 
-local _G = _G;
-local setmetatable, getfenv, setfenv = setmetatable, getfenv, setfenv;
-local type, unpack, select, pairs = type, unpack, select, pairs;
-local min, random = math.min, math.random;
-local format = string.format;
+local _G = _G
+local setmetatable, getfenv, setfenv = setmetatable, getfenv, setfenv
+local type, unpack, select, pairs = type, unpack, select, pairs
+local min, random = math.min, math.random
+local format = string.format
 
-local UnitPower = UnitPower;
-local UnitPowerMax = UnitPowerMax;
-local UnitHealth = UnitHealth;
-local UnitHealthMax = UnitHealthMax;
-local UnitName = UnitName;
-local UnitClass = UnitClass;
-local InCombatLockdown = InCombatLockdown;
-local UnregisterUnitWatch = UnregisterUnitWatch;
-local RegisterUnitWatch = RegisterUnitWatch;
-local RegisterStateDriver = RegisterStateDriver;
-local LOCALIZED_CLASS_NAMES_MALE = LOCALIZED_CLASS_NAMES_MALE;
-local CLASS_SORT_ORDER = CLASS_SORT_ORDER;
-local MAX_RAID_MEMBERS = MAX_RAID_MEMBERS;
+local UnitPower = UnitPower
+local UnitPowerMax = UnitPowerMax
+local UnitHealth = UnitHealth
+local UnitHealthMax = UnitHealthMax
+local UnitName = UnitName
+local UnitClass = UnitClass
+local InCombatLockdown = InCombatLockdown
+local UnregisterUnitWatch = UnregisterUnitWatch
+local RegisterUnitWatch = RegisterUnitWatch
+local RegisterStateDriver = RegisterStateDriver
+local LOCALIZED_CLASS_NAMES_MALE = LOCALIZED_CLASS_NAMES_MALE
+local CLASS_SORT_ORDER = CLASS_SORT_ORDER
+local MAX_RAID_MEMBERS = MAX_RAID_MEMBERS
 
 local attributeBlacklist = {["showRaid"] = true, ["showParty"] = true, ["showSolo"] = true}
 local configEnv
@@ -29,47 +29,48 @@ local originalEnvs = {}
 local overrideFuncs = {}
 
 local function createConfigEnv()
-	if( configEnv ) then return end
+	if configEnv then return end
 	configEnv = setmetatable({
 		UnitPower = function (unit, displayType)
-			if(unit:find("target") or unit:find("focus")) then
-				return UnitPower(unit, displayType);
+			if unit:find("target") or unit:find("focus") then
+				return UnitPower(unit, displayType)
 			end
 
-			return random(1, UnitPowerMax(unit, displayType) or 1);
+			return random(1, UnitPowerMax(unit, displayType) or 1)
 		end,
 		UnitHealth = function(unit)
-			if(unit:find("target") or unit:find("focus")) then
-				return UnitHealth(unit);
+			if unit:find("target") or unit:find("focus") then
+				return UnitHealth(unit)
 			end
 
-			return random(1, UnitHealthMax(unit));
+			return random(1, UnitHealthMax(unit))
 		end,
 		UnitName = function(unit)
-			if(unit:find("target") or unit:find("focus")) then
-				return UnitName(unit);
+			if unit:find("target") or unit:find("focus") then
+				return UnitName(unit)
 			end
-			if(E.CreditsList) then
-				local max = #E.CreditsList;
-				return E.CreditsList[random(1, max)];
+			if E.CreditsList then
+				local max = #E.CreditsList
+				return E.CreditsList[random(1, max)]
 			end
-			return "Test Name";
+			return "Test Name"
 		end,
 		UnitClass = function(unit)
-			if(unit:find("target") or unit:find("focus")) then
-				return UnitClass(unit);
+			if unit:find("target") or unit:find("focus") then
+				return UnitClass(unit)
 			end
 
-			local classToken = CLASS_SORT_ORDER[random(1, #(CLASS_SORT_ORDER))];
-			return LOCALIZED_CLASS_NAMES_MALE[classToken], classToken;
+			local classToken = CLASS_SORT_ORDER[random(1, #(CLASS_SORT_ORDER))]
+			return LOCALIZED_CLASS_NAMES_MALE[classToken], classToken
 		end,
 		Hex = function(r, g, b)
-			if(type(r) == "table") then
-				if(r.r) then r, g, b = r.r, r.g, r.b; else r, g, b = unpack(r); end
+			if type(r) == "table" then
+				if r.r then r, g, b = r.r, r.g, r.b else r, g, b = unpack(r) end
 			end
-			return format("|cff%02x%02x%02x", r*255, g*255, b*255);
+			return format("|cff%02x%02x%02x", r*255, g*255, b*255)
 		end,
 		ColorGradient = ElvUF.ColorGradient,
+		_COLORS = ElvUF.colors
 	}, {
 		__index = _G,
 		__newindex = function(_, key, value) _G[key] = value end,
@@ -101,11 +102,12 @@ local function createConfigEnv()
 end
 
 function UF:ForceShow(frame)
-	if InCombatLockdown() then return; end
+	if InCombatLockdown() then return end
+
 	if not frame.isForced then
 		frame.oldUnit = frame.unit
 		frame.unit = "player"
-		frame.isForced = true;
+		frame.isForced = true
 		frame.oldOnUpdate = frame:GetScript("OnUpdate")
 	end
 
@@ -119,20 +121,19 @@ function UF:ForceShow(frame)
 		frame:Update()
 	end
 
-	if(_G[frame:GetName().."Target"]) then
-		self:ForceShow(_G[frame:GetName().."Target"]);
+	if _G[frame:GetName().."Target"] then
+		self:ForceShow(_G[frame:GetName().."Target"])
 	end
 
-	if(_G[frame:GetName().."Pet"]) then
-		self:ForceShow(_G[frame:GetName().."Pet"]);
+	if _G[frame:GetName().."Pet"] then
+		self:ForceShow(_G[frame:GetName().."Pet"])
 	end
 end
 
 function UF:UnforceShow(frame)
-	if InCombatLockdown() then return; end
-	if not frame.isForced then
-		return
-	end
+	if InCombatLockdown() then return end
+	if not frame.isForced then return end
+
 	frame.forceShowAuras = nil
 	frame.isForced = nil
 
@@ -152,11 +153,11 @@ function UF:UnforceShow(frame)
 		frame:Update()
 	end
 
-	if(_G[frame:GetName().."Target"]) then
+	if _G[frame:GetName().."Target"] then
 		self:UnforceShow(_G[frame:GetName().."Target"])
 	end
 
-	if(_G[frame:GetName().."Pet"]) then
+	if _G[frame:GetName().."Pet"] then
 		self:UnforceShow(_G[frame:GetName().."Pet"])
 	end
 end
@@ -164,7 +165,7 @@ end
 function UF:ShowChildUnits(header, ...)
 	header.isForced = true
 
-	for i=1, select("#", ...) do
+	for i = 1, select("#", ...) do
 		local frame = select(i, ...)
 		frame:RegisterForClicks(nil)
 		frame:SetID(i)
@@ -175,7 +176,7 @@ end
 function UF:UnshowChildUnits(header, ...)
 	header.isForced = nil
 
-	for i=1, select("#", ...) do
+	for i = 1, select("#", ...) do
 		local frame = select(i, ...)
 		frame:RegisterForClicks(self.db.targetOnMouseDown and "AnyDown" or "AnyUp")
 		self:UnforceShow(frame)
@@ -183,7 +184,7 @@ function UF:UnshowChildUnits(header, ...)
 end
 
 local function OnAttributeChanged(self)
-	if not self:GetParent().forceShow and not self.forceShow then return; end
+	if not self:GetParent().forceShow and not self.forceShow then return end
 	if not self:IsShown() then return end
 
 	local db = self.db or self:GetParent().db
@@ -197,7 +198,7 @@ local function OnAttributeChanged(self)
 end
 
 function UF:HeaderConfig(header, configMode)
-	if InCombatLockdown() then return; end
+	if InCombatLockdown() then return end
 
 	createConfigEnv()
 	header.forceShow = configMode
@@ -223,12 +224,12 @@ function UF:HeaderConfig(header, configMode)
 
 		RegisterStateDriver(header, "visibility", header.db.visibility)
 
-		if(header:GetScript("OnEvent")) then
-			header:GetScript("OnEvent")(header, "PLAYER_ENTERING_WORLD");
+		if header:GetScript("OnEvent") then
+			header:GetScript("OnEvent")(header, "PLAYER_ENTERING_WORLD")
 		end
 	end
 
-	for i=1, #header.groups do
+	for i = 1, #header.groups do
 		local group = header.groups[i]
 
 		if group:IsShown() then
@@ -256,36 +257,36 @@ function UF:HeaderConfig(header, configMode)
 		end
 	end
 
-	UF["headerFunctions"][header.groupName]:AdjustVisibility(header);
+	UF.headerFunctions[header.groupName]:AdjustVisibility(header)
 end
 
 function UF:PLAYER_REGEN_DISABLED()
-	for _, header in pairs(UF["headers"]) do
+	for _, header in pairs(UF.headers) do
 		if header.forceShow then
 			self:HeaderConfig(header)
 		end
 	end
 
-	for _, unit in pairs(UF["units"]) do
+	for _, unit in pairs(UF.units) do
 		local frame = self[unit]
 		if frame and frame.forceShow then
 			self:UnforceShow(frame)
 		end
 	end
 
-	for i=1, 5 do
+	for i = 1, 5 do
 		if self["arena"..i] and self["arena"..i].isForced then
 			self:UnforceShow(self["arena"..i])
 		end
 	end
 
-	for i=1, 4 do
+	for i = 1, 4 do
 		if self["boss"..i] and self["boss"..i].isForced then
 			self:UnforceShow(self["boss"..i])
 		end
 	end
 
-	for i=1, 4 do
+	for i = 1, 4 do
 		if self["party"..i] and self["party"..i].isForced then
 			self:UnforceShow(self["party"..i])
 		end
