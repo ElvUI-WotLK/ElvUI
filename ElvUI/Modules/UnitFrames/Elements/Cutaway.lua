beda6eab0bae9b22d561c3378bbfe8506f6bed53
bunny67 <zolov6767@gmail.com>
2019-08-12 17:02:14 +0300

    return comments

--- ElvUI/Modules/UnitFrames/Elements/Cutaway.lua
+++ ElvUI/Modules/UnitFrames/Elements/Cutaway.lua
@@ -1,6 +1,9 @@
 local E, L, V, P, G = unpack(select(2, ...)) --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 local UF = E:GetModule("UnitFrames")
 
+--Lua functions
+--WoW API / Variables
+
 function UF:Construct_Cutaway(frame)
 	local cutaway = {}
 	local frameName = frame:GetName()
