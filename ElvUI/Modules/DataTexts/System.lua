02da225aae240a49291677544a62a268054919d9
Pinya <800pin.ru@gmail.com>
2019-09-15 22:25:08 +0300

    update System datatext

--- ElvUI/Modules/DataTexts/System.lua
+++ ElvUI/Modules/DataTexts/System.lua
@@ -1,11 +1,11 @@
-local E, L, V, P, G = unpack(select(2, ...)); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
+local E, L, V, P, G = unpack(select(2, ...)) --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 local DT = E:GetModule("DataTexts")
 
 --Lua functions
-local select, collectgarbage = select, collectgarbage
-local sort, wipe = table.sort, wipe
+local collectgarbage = collectgarbage
 local floor = math.floor
 local format = string.format
+local tinsert, sort = table.insert, table.sort
 --WoW API / Variables
 local GetAddOnCPUUsage = GetAddOnCPUUsage
 local GetAddOnInfo = GetAddOnInfo
@@ -23,8 +23,7 @@ local ShowUIPanel = ShowUIPanel
 local UpdateAddOnCPUUsage = UpdateAddOnCPUUsage
 local UpdateAddOnMemoryUsage = UpdateAddOnMemoryUsage
 
--- initial delay for update (let the ui load)
-local int, int2 = 6, 5
+local int = 5 -- initial delay
 local statusColors = {
 	"|cff0CD809",
 	"|cffE8DA0F",
@@ -32,19 +31,45 @@ local statusColors = {
 	"|cffD80909"
 }
 
-local enteredFrame = false
+local enteredFrame
 local homeLatencyString = "%d ms"
 local kiloByteString = "%d kb"
 local megaByteString = "%.2f mb"
 
+local memoryTable = {}
+local cpuTable = {}
+local lodTable = {}
+
+local function OnEvent(self, event, addonName)
+	if event == "ADDON_LOADED" then
+		tinsert(memoryTable, lodTable[addonName])
+		tinsert(cpuTable, lodTable[addonName])
+		lodTable[addonName] = nil
+	elseif event == "PLAYER_ENTERING_WORLD" then
+		local _, name, title, enabled
+
+		for i = 1, GetNumAddOns() do
+			name, title, _, enabled = GetAddOnInfo(i)
+
+			if enabled then
+				if IsAddOnLoaded(i) then
+					tinsert(memoryTable, {i, title, 0})
+					tinsert(cpuTable, {i, title, 0})
+				else
+					lodTable[name] = {i, title, 0}
+				end
+			end
+		end
+
+		self:UnregisterEvent(event)
+	end
+end
+
 local function formatMem(memory)
-	local mult = 10 ^ 1
 	if memory > 999 then
-		local mem = ((memory / 1024) * mult) / mult
-		return format(megaByteString, mem)
+		return format(megaByteString, memory / 1024)
 	else
-		local mem = (memory * mult) / mult
-		return format(kiloByteString, mem)
+		return format(kiloByteString, memory)
 	end
 end
 
@@ -54,48 +79,29 @@ local function sortByMemoryOrCPU(a, b)
 	end
 end
 
-local memoryTable = {}
-local cpuTable = {}
-local function RebuildAddonList()
-	local addOnCount = GetNumAddOns()
-	if (addOnCount == #memoryTable) then return end
-
-	-- Number of loaded addons changed, create new memoryTable for all addons
-	wipe(memoryTable)
-	wipe(cpuTable)
-	for i = 1, addOnCount do
-		memoryTable[i] = {i, select(2, GetAddOnInfo(i)), 0}
-		cpuTable[i] = {i, select(2, GetAddOnInfo(i)), 0}
-	end
-end
-
 local function UpdateMemory()
-	-- Update the memory usages of the addons
 	UpdateAddOnMemoryUsage()
-	-- Load memory usage in table
+
 	local totalMemory = 0
 	for i = 1, #memoryTable do
 		memoryTable[i][3] = GetAddOnMemoryUsage(memoryTable[i][1])
 		totalMemory = totalMemory + memoryTable[i][3]
 	end
-	-- Sort the table to put the largest addon on top
+
 	sort(memoryTable, sortByMemoryOrCPU)
 
 	return totalMemory
 end
 
 local function UpdateCPU()
-	--Update the CPU usages of the addons
 	UpdateAddOnCPUUsage()
-	-- Load cpu usage in table
+
 	local totalCPU = 0
 	for i = 1, #cpuTable do
-		local addonCPU = GetAddOnCPUUsage(cpuTable[i][1])
-		cpuTable[i][3] = addonCPU
-		totalCPU = totalCPU + addonCPU
+		cpuTable[i][3] = GetAddOnCPUUsage(cpuTable[i][1])
+		totalCPU = totalCPU + cpuTable[i][3]
 	end
 
-	-- Sort the table to put the largest addon on top
 	sort(cpuTable, sortByMemoryOrCPU)
 
 	return totalCPU
@@ -122,40 +128,41 @@ end
 
 local function OnEnter(self)
 	enteredFrame = true
-	local cpuProfiling = GetCVar("scriptProfile") == "1"
+
 	DT:SetupTooltip(self)
 
 	local totalMemory = UpdateMemory()
-	local _, _, homePing = GetNetStats()
+	local _, _, homeLatency = GetNetStats()
+	local cpuProfiling = GetCVar("scriptProfile") == "1"
 
-	DT.tooltip:AddDoubleLine(L["Home Latency:"], format(homeLatencyString, homePing), 0.69, 0.31, 0.31,0.84, 0.75, 0.65)
+	DT.tooltip:AddDoubleLine(L["Home Latency:"], format(homeLatencyString, homeLatency), 0.69, 0.31, 0.31, 0.84, 0.75, 0.65)
+	DT.tooltip:AddDoubleLine(L["Total Memory:"], formatMem(totalMemory), 0.69, 0.31, 0.31, 0.84, 0.75, 0.65)
 
 	local totalCPU
-	DT.tooltip:AddDoubleLine(L["Total Memory:"], formatMem(totalMemory), 0.69, 0.31, 0.31, 0.84, 0.75, 0.65)
 	if cpuProfiling then
 		totalCPU = UpdateCPU()
 		DT.tooltip:AddDoubleLine(L["Total CPU:"], format(homeLatencyString, totalCPU), 0.69, 0.31, 0.31, 0.84, 0.75, 0.65)
 	end
 
 	DT.tooltip:AddLine(" ")
+
+	local addon, red, green
+
 	if IsShiftKeyDown() or not cpuProfiling then
 		for i = 1, #memoryTable do
-			local ele = memoryTable[i]
-			if ele and IsAddOnLoaded(ele[1]) then
-				local red = ele[3] / totalMemory
-				local green = 1 - red
-				DT.tooltip:AddDoubleLine(ele[2], formatMem(ele[3]), 1, 1, 1, red, green + .5, 0)
-			end
+			addon = memoryTable[i]
+			red = addon[3] / totalMemory
+			green = 1 - red
+			DT.tooltip:AddDoubleLine(addon[2], formatMem(addon[3]), 1, 1, 1, red, green + .5, 0)
 		end
 	else
 		for i = 1, #cpuTable do
-			local ele = cpuTable[i]
-			if ele and IsAddOnLoaded(ele[1]) then
-				local red = ele[3] / totalCPU
-				local green = 1 - red
-				DT.tooltip:AddDoubleLine(ele[2], format(homeLatencyString, ele[3]), 1, 1, 1, red, green + .5, 0)
-			end
+			addon = cpuTable[i]
+			red = addon[3] / totalCPU
+			green = 1 - red
+			DT.tooltip:AddDoubleLine(addon[2], format(homeLatencyString, addon[3]), 1, 1, 1, red, green + .5, 0)
 		end
+
 		DT.tooltip:AddLine(" ")
 		DT.tooltip:AddLine(L["(Hold Shift) Memory Usage"])
 	end
@@ -165,33 +172,29 @@ local function OnEnter(self)
 end
 
 local function OnLeave()
-	enteredFrame = false
+	enteredFrame = nil
 	DT.tooltip:Hide()
 end
 
 local function OnUpdate(self, t)
 	int = int - t
-	int2 = int2 - t
 
 	if int < 0 then
-		RebuildAddonList()
-		int = 10
-	end
-
-	if int2 < 0 then
 		local framerate = floor(GetFramerate() + 0.5)
-		local latency = select(3, GetNetStats())
+		local _, _, homeLatency = GetNetStats()
 
 		self.text:SetFormattedText("FPS: %s%d|r MS: %s%d|r",
 			statusColors[framerate >= 30 and 1 or (framerate >= 20 and framerate < 30) and 2 or (framerate >= 10 and framerate < 20) and 3 or 4],
 			framerate,
-			statusColors[latency < 150 and 1 or (latency >= 150 and latency < 300) and 2 or (latency >= 300 and latency < 500) and 3 or 4],
-			latency)
-		int2 = 1
+			statusColors[homeLatency < 150 and 1 or (homeLatency >= 150 and homeLatency < 300) and 2 or (homeLatency >= 300 and homeLatency < 500) and 3 or 4],
+			homeLatency)
+
+		int = 1
+
 		if enteredFrame then
 			OnEnter(self)
 		end
 	end
 end
 
-DT:RegisterDatatext("System", nil, nil, OnUpdate, OnClick, OnEnter, OnLeave, L["System"])
+DT:RegisterDatatext("System", {"PLAYER_ENTERING_WORLD", "ADDON_LOADED"}, OnEvent, OnUpdate, OnClick, OnEnter, OnLeave, L["System"])
