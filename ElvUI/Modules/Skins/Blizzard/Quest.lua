859ace477341921435454f7a664be1d02392bbbe
Pinya <800pin.ru@gmail.com>
2019-12-19 21:27:58 +0300

    merge Quest and Greeting skins

--- ElvUI/Modules/Skins/Blizzard/Quest.lua
+++ ElvUI/Modules/Skins/Blizzard/Quest.lua
@@ -5,7 +5,7 @@ local S = E:GetModule("Skins")
 local _G = _G
 local select = select
 local unpack = unpack
-local find = string.find
+local find, gsub = string.find, string.gsub
 --WoW API / Variables
 local GetItemInfo = GetItemInfo
 local GetItemQualityColor = GetItemQualityColor
@@ -363,6 +363,33 @@ local function LoadSkin()
 			end
 		end)
 	end
+
+	-- Quest Greeting Frame
+	QuestFrameGreetingPanel:StripTextures(true)
+
+	S:HandleButton(QuestFrameGreetingGoodbyeButton, true)
+	QuestFrameGreetingGoodbyeButton:Point("BOTTOMRIGHT", -37, 4)
+
+	QuestGreetingFrameHorizontalBreak:Kill()
+
+	QuestGreetingScrollFrame:Height(403)
+
+	S:HandleScrollBar(QuestGreetingScrollFrameScrollBar)
+
+	QuestFrameGreetingPanel:HookScript("OnShow", function()
+		GreetingText:SetTextColor(1, 1, 1)
+		CurrentQuestsText:SetTextColor(1, 0.80, 0.10)
+		AvailableQuestsText:SetTextColor(1, 0.80, 0.10)
+
+		for i = 1, MAX_NUM_QUESTS do
+			local button = _G["QuestTitleButton"..i]
+			if button:GetFontString() then
+				if button:GetFontString():GetText() and find(button:GetFontString():GetText(), "|cff000000") then
+					button:GetFontString():SetText(gsub(button:GetFontString():GetText(), "|cff000000", "|cffFFFF00"))
+				end
+			end
+		end
+	end)
 end
 
 S:AddCallback("Skin_Quest", LoadSkin)
