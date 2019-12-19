7edb7223ab894563025430a632e34ae4390ea451
Pinya <800pin.ru@gmail.com>
2019-12-19 21:21:42 +0300

    update Glyph skin
    fix texture aspect ratio
    restore missing animations

--- ElvUI/Modules/Skins/Blizzard/Glyph.lua
+++ ElvUI/Modules/Skins/Blizzard/Glyph.lua
@@ -14,30 +14,37 @@ local function LoadSkin()
 	end
 
 	GlyphFrame:StripTextures()
-	GlyphFrame:CreateBackdrop()
-	GlyphFrame.backdrop:Size(328, 353)
-	GlyphFrame.backdrop:ClearAllPoints()
-	GlyphFrame.backdrop:Point("CENTER", PlayerTalentFrame.backdrop, 0, -16)
 
-	GlyphFrame.texture = GlyphFrame.backdrop:CreateTexture(nil, "OVERLAY")
-	GlyphFrame.texture:SetInside()
-	GlyphFrame.texture:SetTexture("Interface\\Spellbook\\UI-GlyphFrame")
-	GlyphFrame.texture:SetTexCoord(0.0390625, 0.65625, 0.140625, 0.8046875)
+	GlyphFrameBackground:Size(323, 349)
+	GlyphFrameBackground:Point("TOPLEFT", 20, -59)
+	GlyphFrameBackground:CreateBackdrop()
 
+	GlyphFrameBackground:SetTexture("Interface\\Spellbook\\UI-GlyphFrame")
+	GlyphFrameGlow:SetTexture("Interface\\Spellbook\\UI-GlyphFrame-Glow")
+	GlyphFrameGlow:SetParent(GlyphFrameBackground.backdrop)
+	GlyphFrameGlow:SetInside(GlyphFrameBackground.backdrop)
+
+	-- texWidth, texHeight, cropWidth, cropHeight, offsetX, offsetY = 512, 512, 315, 340, 21, 72
+	GlyphFrameBackground:SetTexCoord(0.041015625, 0.65625, 0.140625, 0.8046875)
+
+	-- texWidth, texHeight, cropWidth, cropHeight, offsetX, offsetY = 512, 512, 315, 340, 30, 34
+	GlyphFrameGlow:SetTexCoord(0.05859375, 0.673828125, 0.06640625, 0.73046875)
+
+	local glyphBGScale = 1.0253968
 	local glyphPositions = {
-		{"CENTER", 0, 122},
-		{"CENTER", 0, -127},
-		{"TOPLEFT", 0, -53},
+		{"CENTER", -1, 126},
+		{"CENTER", -1, -119},
+		{"TOPLEFT", 8, -62},
 		{"BOTTOMRIGHT", -10, 70},
-		{"TOPRIGHT", 0, -53},
-		{"BOTTOMLEFT", 10, 70}
+		{"TOPRIGHT", -8, -62},
+		{"BOTTOMLEFT", 7, 70}
 	}
 
 	for i = 1, 6 do
 		local frame = _G["GlyphFrameGlyph"..i]
-		frame:SetScale(1.0379747)
-		frame:SetParent(GlyphFrame.backdrop)
-		frame:SetPoint(unpack(glyphPositions[i]))
+		frame:SetParent(GlyphFrameBackground.backdrop)
+		frame:SetScale(glyphBGScale)
+		frame:Point(unpack(glyphPositions[i]))
 	end
 
 	GlyphFrame:HookScript("OnShow", function()
@@ -59,6 +66,32 @@ local function LoadSkin()
 			PlayerTalentFramePreviewBar:Hide()
 		end
 	end)
+
+	do
+		local slotAnimations = {}
+		local TOPLEFT, TOP, TOPRIGHT, BOTTOMRIGHT, BOTTOM, BOTTOMLEFT = 3, 1, 5, 4, 2, 6
+		slotAnimations[TOPLEFT] = {["point"] = "CENTER", ["xStart"] = -13, ["xStop"] = -85, ["yStart"] = 17, ["yStop"] = 60}
+		slotAnimations[TOP] = {["point"] = "CENTER", ["xStart"] = -13, ["xStop"] = -13, ["yStart"] = 17, ["yStop"] = 100}
+		slotAnimations[TOPRIGHT] = {["point"] = "CENTER", ["xStart"] = -13, ["xStop"] = 59, ["yStart"] = 17, ["yStop"] = 60}
+		slotAnimations[BOTTOM] = {["point"] = "CENTER", ["xStart"] = -13, ["xStop"] = -13, ["yStart"] = 17, ["yStop"] = -64}
+		slotAnimations[BOTTOMLEFT] = {["point"] = "CENTER", ["xStart"] = -13, ["xStop"] = -87, ["yStart"] = 18, ["yStop"] = -27}
+		slotAnimations[BOTTOMRIGHT] = {["point"] = "CENTER", ["xStart"] = -13, ["xStop"] = 61, ["yStart"] = 18, ["yStop"] = -27}
+
+		for _, animData in pairs(slotAnimations) do
+			animData.xStart = animData.xStart + 3
+			animData.yStart = animData.yStart + 8
+			animData.xStop = (animData.xStop + 3) * glyphBGScale
+			animData.yStop = (animData.yStop + 8) * glyphBGScale
+		end
+
+		hooksecurefunc("GlyphFrame_StartSlotAnimation", function(slotID, duration, size)
+			local sparkle = _G["GlyphFrameSparkle"..slotID]
+			local animation = slotAnimations[slotID]
+
+			sparkle:SetPoint("CENTER", GlyphFrame, animation.point, animation.xStart, animation.yStart)
+			sparkle.animGroup.translate:SetOffset(animation.xStop - animation.xStart, animation.yStop - animation.yStart)
+		end)
+	end
 end
 
 S:AddCallbackForAddon("Blizzard_GlyphUI", "Skin_Blizzard_GlyphUI", LoadSkin)
