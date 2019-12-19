9706f8f2cd5e66a80d6181dfedde439f57bc6206
Pinya <800pin.ru@gmail.com>
2019-12-19 21:24:09 +0300

    update skins: LFD, LFR

--- ElvUI/Modules/Skins/Blizzard/LFR.lua
+++ ElvUI/Modules/Skins/Blizzard/LFR.lua
@@ -15,28 +15,26 @@ local function LoadSkin()
 	LFRParentFrame.backdrop:Point("TOPLEFT", 11, -12)
 	LFRParentFrame.backdrop:Point("BOTTOMRIGHT", -3, 4)
 
-	S:SetUIPanelWindowInfo(LFRParentFrame, "width", 341)
+	S:HookScript(LFRParentFrame, "OnShow", function(self)
+		S:SetUIPanelWindowInfo(self, "width")
+		S:Unhook(self, "OnShow")
+	end)
+
+	S:HandleCloseButton((LFRParentFrame:GetChildren()), LFRParentFrame.backdrop)
 
 	LFRQueueFrame:StripTextures()
 	LFRBrowseFrame:StripTextures()
 
 	local buttons = {
-		"LFRQueueFrameFindGroupButton",
-		"LFRQueueFrameAcceptCommentButton",
-		"LFRBrowseFrameSendMessageButton",
-		"LFRBrowseFrameInviteButton",
-		"LFRBrowseFrameRefreshButton"
+		LFRQueueFrameFindGroupButton,
+		LFRQueueFrameAcceptCommentButton,
+		LFRBrowseFrameSendMessageButton,
+		LFRBrowseFrameInviteButton,
+		LFRBrowseFrameRefreshButton,
+		LFRQueueFrameNoLFRWhileLFDLeaveQueueButton
 	}
 	for i = 1, #buttons do
-		S:HandleButton(_G[buttons[i]], true)
-	end
-
-	--Close button doesn't have a fucking name, extreme hackage
-	for i = 1, LFRParentFrame:GetNumChildren() do
-		local child = select(i, LFRParentFrame:GetChildren())
-		if child.GetPushedTexture and child:GetPushedTexture() and not child:GetName() then
-			S:HandleCloseButton(child, LFRParentFrame.backdrop)
-		end
+		S:HandleButton(buttons[i], true)
 	end
 
 	S:HandleTab(LFRParentFrameTab1)
@@ -46,7 +44,16 @@ local function LoadSkin()
 	S:HandleScrollBar(LFRQueueFrameSpecificListScrollFrameScrollBar)
 
 	LFRQueueFrameCommentTextButton:CreateBackdrop("Default")
-	LFRQueueFrameCommentTextButton:Height(35)
+
+	--DPS, Healer, Tank check button's don't have a name, use it's parent as a referance.
+	S:HandleCheckBox((LFRQueueFrameRoleButtonTank:GetChildren()))
+	S:HandleCheckBox((LFRQueueFrameRoleButtonHealer:GetChildren()))
+	S:HandleCheckBox((LFRQueueFrameRoleButtonDPS:GetChildren()))
+	LFRQueueFrameRoleButtonTank:GetChildren():SetFrameLevel(LFRQueueFrameRoleButtonTank:GetChildren():GetFrameLevel() + 2)
+	LFRQueueFrameRoleButtonHealer:GetChildren():SetFrameLevel(LFRQueueFrameRoleButtonHealer:GetChildren():GetFrameLevel() + 2)
+	LFRQueueFrameRoleButtonDPS:GetChildren():SetFrameLevel(LFRQueueFrameRoleButtonDPS:GetChildren():GetFrameLevel() + 2)
+
+	LFRQueueFrameSpecificListScrollFrame:StripTextures()
 
 	for i = 1, 7 do
 		local button = "LFRBrowseFrameColumnHeader"..i
@@ -75,15 +82,21 @@ local function LoadSkin()
 		end)
 	end
 
-	--DPS, Healer, Tank check button's don't have a name, use it's parent as a referance.
-	S:HandleCheckBox(LFRQueueFrameRoleButtonTank:GetChildren())
-	S:HandleCheckBox(LFRQueueFrameRoleButtonHealer:GetChildren())
-	S:HandleCheckBox(LFRQueueFrameRoleButtonDPS:GetChildren())
-	LFRQueueFrameRoleButtonTank:GetChildren():SetFrameLevel(LFRQueueFrameRoleButtonTank:GetChildren():GetFrameLevel() + 2)
-	LFRQueueFrameRoleButtonHealer:GetChildren():SetFrameLevel(LFRQueueFrameRoleButtonHealer:GetChildren():GetFrameLevel() + 2)
-	LFRQueueFrameRoleButtonDPS:GetChildren():SetFrameLevel(LFRQueueFrameRoleButtonDPS:GetChildren():GetFrameLevel() + 2)
+	LFRQueueFrameNoLFRWhileLFD:Size(325, 271)
+	LFRQueueFrameNoLFRWhileLFD:Point("BOTTOMRIGHT", -11, 41)
 
-	LFRQueueFrameSpecificListScrollFrame:StripTextures()
+	LFRQueueFrameComment:Width(323)
+	LFRQueueFrameComment:Point("TOPLEFT", LFRQueueFrame, "BOTTOMLEFT", 20, 74)
+
+	LFRQueueFrameCommentTextButton:Size(323, 32)
+
+	LFRQueueFrameFindGroupButton:Point("BOTTOMLEFT", 19, 12)
+	LFRQueueFrameAcceptCommentButton:Point("BOTTOMRIGHT", -11, 12)
+	LFRBrowseFrameSendMessageButton:Point("BOTTOMLEFT", 19, 12)
+	LFRBrowseFrameInviteButton:Point("LEFT", LFRBrowseFrameSendMessageButton, "RIGHT", 4, 0)
+	LFRBrowseFrameRefreshButton:Point("LEFT", LFRBrowseFrameInviteButton, "RIGHT", 4, 0)
+
+	LFRParentFrameTab1:Point("BOTTOMLEFT", 11, -26)
 end
 
 S:AddCallback("Skin_LFR", LoadSkin)
