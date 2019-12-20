430017d6d946a49c60bb9097b7939f134e29df45
Pinya <800pin.ru@gmail.com>
2019-12-20 21:03:56 +0300

    spaces to tabs

--- ElvUI/Core/StaticPopups.lua
+++ ElvUI/Core/StaticPopups.lua
@@ -466,10 +466,10 @@ E.PopupDialogs.SCRIPT_PROFILE = {
 }
 
 E.PopupDialogs.ELVUI_CONFIG_FOUND = {
-    text = L["You still have ElvUI_Config installed. ElvUI_Config has been renamed to ElvUI_OptionsUI, please remove it."],
-    button1 = ACCEPT,
-    whileDead = 1,
-    hideOnEscape = false
+	text = L["You still have ElvUI_Config installed. ElvUI_Config has been renamed to ElvUI_OptionsUI, please remove it."],
+	button1 = ACCEPT,
+	whileDead = 1,
+	hideOnEscape = false
 }
 
 local MAX_STATIC_POPUPS = 4
