7885d91b60a331f5e62b85724aa3c42ed037eda5
Pinya <800pin.ru@gmail.com>
2020-04-04 11:25:19 +0300

    Update Durability.lua
    adjust texture position of the durability frame

--- ElvUI/Modules/Blizzard/Durability.lua
+++ ElvUI/Modules/Blizzard/Durability.lua
@@ -4,16 +4,24 @@ local B = E:GetModule("Blizzard")
 --Lua functions
 --WoW API / Variables
 
-local function SetPosition(self, _, parent)
-	if parent == "MinimapCluster" or parent == MinimapCluster then
-		self:ClearAllPoints()
-		self:Point("RIGHT", Minimap, "RIGHT")
-		self:SetScale(0.6)
-	end
-end
-
 function B:PositionDurabilityFrame()
 	DurabilityFrame:SetFrameStrata("HIGH")
+	DurabilityFrame:SetScale(0.6)
 
-	hooksecurefunc(DurabilityFrame, "SetPoint", SetPosition)
+	DurabilityWeapon:Point("RIGHT", DurabilityWrists, "LEFT", 6, 0)
+	DurabilityShield:Point("LEFT", DurabilityWrists, "RIGHT", -6, 10)
+	DurabilityOffWeapon:Point("LEFT", DurabilityWrists, "RIGHT", -6, 0)
+	DurabilityRanged:Point("TOP", DurabilityShield, "BOTTOM", -1, 0)
+
+	hooksecurefunc(DurabilityFrame, "SetPoint", function(self, _, point)
+		if point ~= Minimap then
+			self:ClearAllPoints()
+
+			if DurabilityShield:IsShown() or DurabilityOffWeapon:IsShown() or DurabilityRanged:IsShown() then
+				self:Point("RIGHT", Minimap, "RIGHT", -7, 0)
+			else
+				self:Point("RIGHT", Minimap, "RIGHT", 8, 0)
+			end
+		end
+	end)
 end
