ea70301e28c383bc340497950b1bde0bb9a07480
Pinya <800pin.ru@gmail.com>
2021-06-14 16:43:55 +0300

    Skins: adjust HandleScrollBar to skin horizontal and anonymous scrollbars

--- ElvUI/Modules/Skins/Skins.lua
+++ ElvUI/Modules/Skins/Skins.lua
@@ -114,12 +114,24 @@ function S:HandleScrollBar(frame, horizontal)
 	local scrollUpButton, scrollDownButton
 	local thumb = frame.thumbTexture or frame.GetThumbTexture and frame:GetThumbTexture() or _G[format("%s%s", frameName, "ThumbTexture")]
 
-	if not horizontal then
-		scrollUpButton = parent.scrollUp or _G[format("%s%s", frameName, "ScrollUpButton")] or _G[format("%s%s", frameName, "UpButton")] or _G[format("%s%s", frameName, "ScrollUp")]
-		scrollDownButton = parent.scrollDown or _G[format("%s%s", frameName, "ScrollDownButton")] or _G[format("%s%s", frameName, "DownButton")] or _G[format("%s%s", frameName, "ScrollDown")]
+	if frameName then
+		if not horizontal then
+			scrollUpButton = parent.scrollUp or _G[format("%s%s", frameName, "ScrollUpButton")] or _G[format("%s%s", frameName, "UpButton")] or _G[format("%s%s", frameName, "ScrollUp")]
+			scrollDownButton = parent.scrollDown or _G[format("%s%s", frameName, "ScrollDownButton")] or _G[format("%s%s", frameName, "DownButton")] or _G[format("%s%s", frameName, "ScrollDown")]
+		else
+			scrollUpButton = _G[format("%s%s", frameName, "ScrollLeftButton")] or _G[format("%s%s", frameName, "LeftButton")] or _G[format("%s%s", frameName, "ScrollLeft")]
+			scrollDownButton = _G[format("%s%s", frameName, "ScrollRightButton")] or _G[format("%s%s", frameName, "RightButton")] or _G[format("%s%s", frameName, "ScrollRight")]
+		end
 	else
-		scrollUpButton = _G[format("%s%s", frameName, "ScrollLeftButton")] or _G[format("%s%s", frameName, "LeftButton")] or _G[format("%s%s", frameName, "ScrollLeft")]
-		scrollDownButton = _G[format("%s%s", frameName, "ScrollRightButton")] or _G[format("%s%s", frameName, "RightButton")] or _G[format("%s%s", frameName, "ScrollRight")]
+		for i, child in ipairs({frame:GetChildren()}) do
+			if child:GetObjectType() == "Button" then
+				if child:GetNormalTexture():GetTexture() == [[Interface\Buttons\UI-ScrollBar-ScrollUpButton-Up]] then
+					scrollUpButton = child
+				elseif child:GetNormalTexture():GetTexture() == [[Interface\Buttons\UI-ScrollBar-ScrollDownButton-Up]] then
+					scrollDownButton = child
+				end
+			end
+		end
 	end
 
 	if not horizontal then
